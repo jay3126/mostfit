3c237a67d7c3f666bf012dbaeab517a31cbe678e
krishnan <krishnan@pragya.(none)>
2010-09-07 10:54:34 +0530

    Refinements to guarantor and minor corrections

--- app/models/guarantor.rb
+++ app/models/guarantor.rb
@@ -1,19 +1,16 @@
-
 class Guarantor
   include DataMapper::Resource
 
-  RELATIONS = ['Spouse', 'Father', 'Mother', 'Adult Son', 'Other']
-  DOC = ['voter_id', 'driving_licence', 'pan_card', 'gp_certificate', 'ration_card', 'nrega_card', 'phone_bill','electricity_bill']
   property :id,   Serial
+  property :legacy_id, String 
   property :name, String, :index => true
   property :father_name, String, :index => true
-  property :date_of_birth, Date, :nullable => false, :default => Date.today
+  property :date_of_birth, Date
   property :address, String 
-  property :relationship_to_client, Enum.send('[]',*RELATIONS),:default => '', :nullable => true, :lazy => true
-  property :external_id_number, String, :index => true
-  property :external_id_type, Enum.send('[]',*DOC), :default => '', :nullable => true, :lazy => true
+  property :relationship_to_client, Enum.send('[]', *['', 'spouse', 'brother', 'brother_in_law', 'father', 'father_in_law', 'adult_son', 'other']), :default => '', :nullable => true, :lazy => true
  
   belongs_to :client
+  belongs_to :guarantor_occupation, :nullable => true, :child_key => [:guarantor_occupation_id], :model => 'Occupation'  
   validates_present :name
   validates_present :father_name
   validates_length :name,   :minimum => 3
