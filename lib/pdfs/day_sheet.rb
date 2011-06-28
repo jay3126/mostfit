85cadda4d76e2540943f013f9bd18dbbe7cd24e1
Koustubh Sinkar <ksinkar@gmail.com>
2011-06-28 16:31:44 +0530

    Support #893: Number of columns reduced in the Daily Collection Sheet PDF

--- lib/pdfs/day_sheet.rb
+++ lib/pdfs/day_sheet.rb
@@ -52,9 +52,8 @@ module Pdf
               number_of_installments = loan.number_of_installments_before(date)
               
               table.data.push({"name" => client.name, "loan id" => loan.id, "amount" => loan.amount.to_currency, 
-                                "outstanding" => actual_outstanding.to_currency, "status" => lh.status.to_s,                                
-                                "disbursed" => loan.disbursal_date.to_s, "installment" =>  number_of_installments,
-                                "principal" => principal_due.to_currency, "interest" => interest_due.to_currency, "days absent/total" => (days_absent[client.id]||0).to_s / (days_present[client.id]||0).to_s,"fee" => fee.to_currency, "total due" =>  total_due.to_currency, "signature" => "" })
+                                "outstanding" => actual_outstanding.to_currency, "installment" =>  number_of_installments,
+                                "total due" =>  total_due.to_currency, "signature" => "" })
               group_amount       += loan.amount
               group_outstanding  += actual_outstanding
               group_installments += number_of_installments
@@ -68,8 +67,7 @@ module Pdf
             end
           } #clients end
           table.data.push({"amount" => group_amount.to_currency, "outstanding" => group_outstanding.to_currency,
-                            "principal" => group_principal.to_currency, "interest" => group_interest.to_currency,
-                            "fee" => group_fee.to_currency, "total due" => group_due.to_currency                            
+                           "total due" => group_due.to_currency                            
                           })
           tot_amount         += group_amount
           tot_outstanding    += group_outstanding
@@ -80,12 +78,10 @@ module Pdf
           total_due          += (group_principal + group_interest + group_fee)
         } #groups end
         table.data.push({"amount" => tot_amount.to_currency, "outstanding" => tot_outstanding.to_currency,
-                          "principal" => tot_principal.to_currency,
-                          "interest" => tot_interest.to_currency, "fee" => tot_fee.to_currency,
                           "total due" => (tot_principal + tot_interest + tot_fee).to_currency
                         })
         
-        table.column_order  = ["name", "loan id" , "amount", "outstanding", "status", "disbursed", "installment", "principal", "interest", "fee", "total due", "days absent/total", "signature"]
+        table.column_order  = ["name", "loan id" , "amount", "outstanding", "installment", "total due", "signature"]
         table.show_lines    = :all
         table.show_headings = true
         table.shade_rows    = :none
