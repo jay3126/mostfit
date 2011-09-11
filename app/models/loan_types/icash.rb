bdd7d8b2ee6b5583199fb63e229d1cd747b20614
Siddharth <svs@intellecap.net>
2011-09-11 17:16:51 +0530

    icash loan types

--- app/models/loan_types/icash.rb
+++ app/models/loan_types/icash.rb
@@ -1,5 +1,91 @@
 module Mostfit
   module PaymentStyles
+
+    module EquatedWeeklyRoundedNewInterest
+      # This loan product recalculates interest based on the new balances after rounding.
+      include ExcelFormula
+      # property :purpose,  String
+
+      def self.display_name
+        "Reducing balance schedule with new interest (Equated Weekly)"
+      end
+
+      def scheduled_principal_for_installment(number)
+        # number unused in this implentation, subclasses may decide differently
+        # therefor always supply number, so it works for all implementations
+        raise "number out of range, got #{number} but max is #{number_of_installments}" if number < 0 or number > number_of_installments
+        return reducing_schedule[number][:principal_payable]
+      end
+
+      def scheduled_interest_for_installment(number)  # typically reimplemented in subclasses
+        # number unused in this implentation, subclasses may decide differently
+        # therefor always supply number, so it works for all implementations
+        raise "number out of range, got #{number}" if number < 0 or number > number_of_installments
+        return reducing_schedule[number][:interest_payable]
+      end
+
+      def equated_payment
+        payment            = pmt(interest_rate/get_divider, number_of_installments, amount, 0, 0)
+        rnd = loan_product.rounding || 1
+        (payment / rnd).send(loan_product.rounding_style) * rnd
+      end    
+      
+      def pay_prorata(total, received_on, curr_bal = nil)
+        #adds up the principal and interest amounts that can be paid with this amount and prorates the amount
+        i = used = prin = int = 0.0
+        d = received_on
+        total = total.to_f
+        prin_due = info(d)[:principal_due]
+        int_due  = info(d)[:interest_due]
+        if prin_due and int_due
+          prin = prin_due
+          int = int_due
+          used += (int + prin)
+        end
+        while used < total
+          prin += scheduled_principal_for_installment(installment_for_date(d)).round(2)
+          int  += scheduled_interest_for_installment(installment_for_date(d)).round(2)
+          used  = (prin + int)
+          d = shift_date_by_installments(d, 1)
+        end
+        interest  = total * int/(prin + int)
+        principal = total * prin/(prin + int)
+        [interest, principal]
+      end
+
+
+      private
+      def reducing_schedule
+        return @reducing_schedule if @reducing_schedule
+        @reducing_schedule = {}    
+        balance = amount
+        actual_payment = equated_payment
+        1.upto(number_of_installments){|installment|
+          @reducing_schedule[installment] = {}
+          @reducing_schedule[installment][:interest_payable]  = ((balance * interest_rate) / get_divider)
+          @reducing_schedule[installment][:principal_payable] = [(actual_payment - @reducing_schedule[installment][:interest_payable]), balance].min
+          balance = balance - @reducing_schedule[installment][:principal_payable]
+        }
+        return @reducing_schedule
+      end
+
+      def get_divider
+        case installment_frequency
+        when :weekly
+          52
+        when :biweekly
+          26
+        when :fortnightly
+          26
+        when :monthly
+          12
+        when :daily
+          365
+        end    
+      end
+    end
+
+
     module DairyLoan
       
       def self.display_name
@@ -184,6 +270,32 @@ module Mostfit
         raise "number out of range, got #{number} but max is #{number_of_installments}" if number < 0 or number > number_of_installments
         return reducing_schedule[number][:interest_payable]
       end
+
+      def pay_prorata(total, received_on, curr_bal = nil)
+        #adds up the principal and interest amounts that can be paid with this amount and prorates the amount
+        i = used = prin = int = 0.0
+        d = received_on
+        total = total.to_f
+        prin_due = info(d)[:principal_due]
+        int_due  = info(d)[:interest_due]
+        if prin_due and int_due
+          prin = prin_due
+          int = int_due
+          used += (int + prin)
+        end
+        while used < total
+          prin += scheduled_principal_for_installment(installment_for_date(d)).round(2)
+          int  += scheduled_interest_for_installment(installment_for_date(d)).round(2)
+          used  = (prin + int)
+          d = shift_date_by_installments(d, 1)
+        end
+        interest  = total * int/(prin + int)
+        principal = total * prin/(prin + int)
+        [interest, principal]
+      end
+
+
+
     end
 
   end
