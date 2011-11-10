8de92b49fd97502bb66c3abd48ce8f082fc8e1a9
jay3126 <janmejay.rai@intellecap.com>
2011-11-10 14:54:40 +0530

    daily report link added back

--- app/controllers/reports.rb
+++ app/controllers/reports.rb
@@ -1,7 +1,7 @@
 class Reports < Application
   include DateParser
   Types = {
-    :periodic     => [WeeklyReport, DailyTransactionSummary], 
+    :periodic     => [DailyReport, WeeklyReport, DailyTransactionSummary], 
     :consolidated => [ConsolidatedReport, StaffConsolidatedReport, QuarterConsolidatedReport, AggregateConsolidatedReport], 
     :registers    => [TransactionLedger, LoanSanctionRegister, LoanDisbursementRegister, ScheduledDisbursementRegister, ClaimReport, InsuranceRegister, PortfolioAllocationReport, OfflinePayments, OfflineAttendance], 
     :targets_and_projections  => [CashProjectedReport, ProjectedReport, TargetReport, StaffTargetReport, MonthlyTargetReport, IncentiveReport],
