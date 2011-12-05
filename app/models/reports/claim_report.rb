d2c2c3e9ae79668a80fd0e6cb4d2a168b4b6e73f
Siddharth Sharma <svs@intellecap.net>
2011-12-06 02:25:37 +0530

    checked out production version of claim report. numbers are matching now

--- app/models/reports/claim_report.rb
+++ app/models/reports/claim_report.rb
@@ -9,29 +9,13 @@ class ClaimReport < Report
   end
 
   def self.name
-    "Claim Report"
-  end
-
-  def name
-    "Claim Report from #{@from_date} to #{@to_date}"
+    "Claim Report "
   end
 
   def generate
     branches, centers, claims = {}, {}, {}
-
-    #These 4 params are to implement 4 conditions:-
-    #IF Date of death OR Date of Submission of Claim OR Date of receipt of claim from insurance company OR Date of payment to client
-    #then the data for claim is to be displyed.
-
-    params = {:claim_submission_date.gte => @from_date, :claim_submission_date.lte => @to_date, :order => [:claim_submission_date]}
-    params1 = {:date_of_death.gte => @from_date, :date_of_death.lte => @to_date, :order => [:date_of_death]}
-    params2 = {:receipt_of_claim_on.gte => @from_date, :receipt_of_claim_on.lte => @to_date, :order => [:receipt_of_claim_on]}
-    params3 = {:payment_to_client_on.gte => @from_date, :payment_to_client_on.lte => @to_date, :order => [:payment_to_client_on]}
- 
-    #claims = (Claim.all(params) + Claim.all(:claim_submission_date => nil, :order => [:claim_submission_date]))
-
-    #if any 1 of the below condition is fulfilled then display the data.
-    claims = Claim.all(params) || Claim.all(params1) || Claim.all(params2) || Claim.all(params3)
-    claims.group_by{ |claim| claim.client.center.branch} 
+    params = {:claim_submission_date.gte => from_date, :claim_submission_date.lte => to_date, :order => [:claim_submission_date]}
+    Claim.all(params) + Claim.all(:claim_submission_date => nil, :order => [:claim_submission_date])
+    Claim.all.group_by{ |claim| claim.client.center.branch} 
   end
 end
