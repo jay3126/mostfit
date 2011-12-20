109aaf99bdd6c1ca4104cd4b043724bdaf171774
jay3126 <janmejay.rai@intellecap.com>
2011-12-20 17:37:15 +0530

    Revert "Insurance Register modified according to Moral needs"
    This reverts commit 4997211f6a19ac1d86fa115374d67d1c5cb87503.
    Changes already done so reverting this commit back to it's original state.

--- app/models/reports/insurance_register.rb
+++ app/models/reports/insurance_register.rb
@@ -1,9 +1,11 @@
 class InsuranceRegister < Report
-  attr_accessor :date
+  attr_accessor :from_date, :to_date, :branch, :branch_id
+
 
   def initialize(params, dates, user)
-    @date = (dates and dates[:date]) ? dates[:date] : Date.today
-    @name   = "Insurance Report of #{date}"
+    @from_date = (dates and dates[:from_date]) ? dates[:from_date] : Date.today
+    @to_date   = (dates and dates[:to_date]) ? dates[:to_date] : Date.today
+    @name   = "Report from #{@from_date} to #{@to_date}"
     get_parameters(params, user)
   end
 
@@ -12,53 +14,16 @@ class InsuranceRegister < Report
   end
 
   def name
-    "Insurance Report of #{date}"
+    "Insurance Register from #{@from_date} to #{@to_date}"
   end
 
   def generate
-    #this report has been modified according to Moral needs.
-    data = {}
-
-    Loan.all(:disbursal_date => @date).each do |l|
-
-      loan_id = l.id
-      loan_amount = l.amount
-      loan_disbursal_date = l.disbursal_date
-
-      client = Client.get(l.client_id)
-      client_id = l.client_id
-      client_name = client.name
-      if client.date_of_birth
-        client_age = (Time.now.year - client.date_of_birth.year)
-      else
-        client_age = "NA"
-      end
-
-      if client.spouse_name
-        client_spouse_name = client.spouse_name
-      else
-        client_spouse_name = "NA"
-      end
-
-      if client.spouse_date_of_birth
-        client_spouse_age = (Time.now.year - client.spouse_date_of_birth.year)
-      else
-        client_spouse_age = "NA"
-      end
-
-      center = Center.get(l.c_center_id)
-      center_id = l.c_center_id
-      center_name = center.name
-
-      branch = Branch.get(l.c_branch_id)
-      branch_id = l.c_branch_id
-      branch_name = branch.name
-
-      data[l.id] = {:loan_id => loan_id, :loan_amount => loan_amount, :loan_disbursal_date => loan_disbursal_date, :client_id => client_id,
-      :client_name => client_name, :client_age => client_age, :client_spouse_name => client_spouse_name, :client_spouse_age => client_spouse_age,
-      :center_id => center_id, :center_name => center_name, :branch_id => branch_id, :branch_name => branch_name}
-    end
-    return data
+    hash = {:disbursal_date => @from_date..@to_date}.merge(@branch_id ? {:c_branch_id => @branch_id} : {})
+    loans = Loan.all(hash)
+    clients = Client.all(:id => loans.aggregate(:client_id)).map{|c| [c.id, {:client => c, :loans => []}]}.to_hash
+    debugger
+    loans.each{|l| clients[l.client_id][:loans].push(l)}
+    return clients
   end
 
   def branch_should_be_selected
