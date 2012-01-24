6d5308f655267ec13f3ddb2c154b3214f746bb4f
Koustubh Sinkar <ksinkar@gmail.com>
2012-01-24 18:19:06 +0530

    Bug #1836: Default funding line selection is now more intuitive
        "Allow choice for funding line in bulk loan creation?"
    has now been replaced by
        "Set a default funding line in bulk loan creation?"
    
    The dropdown for the funding lines appears only when the above option is
    enabled.

--- app/models/mfi.rb
+++ app/models/mfi.rb
@@ -61,7 +61,7 @@ class Mfi
 
   property :main_text, Text, :nullable => true, :lazy => true
   
-  property :allow_choice_of_funding_line, Boolean, :default => false, :index => true
+  property :set_default_funding_line, Boolean, :default => false, :index => true
   has 1, :funding_line, :child_key => [:funding_line_id]
   property :funding_line_id, Integer, :nullable => true
 
