a6f87a12eaaefab0ff61271dea45454863a198f5
Siddharth Sharma <svs@intellecap.net>
2012-01-27 00:53:01 +0530

    Fix issue of getting wrong rows in payments by staff member
    Also fixed LoanHistory#latest_by_status

--- app/controllers/data_entry/payments.rb
+++ app/controllers/data_entry/payments.rb
@@ -98,8 +98,7 @@ module DataEntry
             @centers                         = Center.all(:manager => @staff_member)
             
             # select loans
-            debugger
-            @loan_histories                  = LoanHistory.latest_by_status(:center_id => @centers.aggregate(:id), :date => @date, :status => :outstanding, :principal_due.gt => 0)
+            @loan_histories                  = LoanHistory.all(:center_id => @centers.aggregate(:id), :date => @date, :status => :outstanding, :principal_due.gt => 0)
             @loans                           = @loan_histories.loans
             @clients                         = @loans.clients
             @centers                         = @clients.centers
