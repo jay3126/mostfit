2331ee20a0a7daed1402644783df0642d68a528c
Krishnan Mani <km@krishnanm.com>
2012-02-18 15:07:33 +0530

    DirtyLoan is causing so much trouble on production that we can't risk running it for the moment

--- app/models/dirty_loan.rb
+++ app/models/dirty_loan.rb
@@ -11,57 +11,16 @@ class DirtyLoan
   belongs_to :loan
 
   def self.add(loan)
-    if loan.is_a? Loan
-      dirty = DirtyLoan.first_or_create(:loan => loan, :cleaned_at => nil)      
-    elsif loan.is_a? Integer
-      dirty = DirtyLoan.first_or_create(:loan_id => loan, :cleaned_at => nil)      
-    end
-    @@poke_thread = true
+    false
   end
 
   def self.clear(id=nil)
-    hash = id ? {:id => id} : {}
-    DirtyLoan.pending(hash).aggregate(:id).each{|_dl|
-      dl = DirtyLoan.get(_dl)
-      if not dl.cleaning_started or (Time.now.to_time - dl.cleaning_started.to_time > 14400)
-        dl.cleaning_started = Time.now
-        dl.save
-      end
-      begin
-        break unless @@poke_thread
-        dl.loan.update_history(true)
-        dl.cleaned_at = Time.now
-        dl.save
-      rescue Exception => e
-        puts e.message
-        puts e.backtrace
-      end
-    }
-    @@poke_thread = false if pending.length ==  0
-    return true
   end
 
   def self.pending(hash = {})
-    DirtyLoan.all({:cleaned_at => nil}.merge(hash))
+    []
   end
 
   def self.start_thread
-    cleaner_interval = Kernel.const_defined?("CLEANER_INTERVAL") ? CLEANER_INTERVAL : 300
-    if true
-      Thread.new{
-        counter = 0
-        while true
-          if @@poke_thread or counter == cleaner_interval
-            self.clear
-            counter = 0
-          end
-          sleep 30
-          counter += 10
-        end        
-      }
-      return true
-    else
-      return false
-    end
   end
 end
