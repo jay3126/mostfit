cfe1e59e742e4ac7b53d0335cc0238e77133c9a7
Samarendra M Hedaoo <knightsamar@gmail.com>
2012-03-02 10:38:18 +0530

    Re-enabling demographic details tab in Client page.
    Partial fix for #757

--- app/models/constants.rb
+++ app/models/constants.rb
@@ -2,4 +2,9 @@ module Constants
   module Center
   CENTER_CATEGORIES = ['','urban','rural']
   end
+  
+  module Client
+  RELIGIONS = ['Hindu','Muslim','Sikh','Christian','Jain','Buddha']
+  CASTES = ['General','SC','ST','OBC']
+  end
 end
