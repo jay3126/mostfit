c4fb13bb498946ccc57347b4aa3a456c06db0efb
jay3126 <janmejay.rai@intellecap.com>
2012-03-05 14:50:49 +0530

    default dates set for from_date and to_date on insurance_policy/fields page

--- app/models/insurance_policy.rb
+++ app/models/insurance_policy.rb
@@ -9,8 +9,8 @@ class InsurancePolicy
   property :policy_no, String, :nullable => true
   property :sum_insured, Integer, :nullable => false
   property :premium, Integer, :nullable => false
-  property :date_from, Date, :nullable => false
-  property :date_to, Date, :nullable => false
+  property :date_from, Date, :nullable => false, :default => Date.today
+  property :date_to, Date, :nullable => false,   :default => Date.today+365
   property :nominee, String, :nullable => true
   property :status, Enum.send("[]", *POLICY_STATUSES), :nullable => true
   property :cover_for, Enum.send("[]", *COVER_FOR), :nullable => true, :default => 'self'
