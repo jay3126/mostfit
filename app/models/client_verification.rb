4e184ca8d1ee30a4dfa5c1440678a8621668c287
Krishnan Mani <km@krishnanm.com>
2012-03-09 05:30:52 +0530

    Enhanced the client verification model for CPVs
    The client verification uses a primary key that consists of the loan application id, the verification type, and verification status
    The constants have now been changed to strings instead of symbols for easier integration with the user interface
    More spec tests have been added to cover the basic model validations

--- app/models/client_verification.rb
+++ app/models/client_verification.rb
@@ -2,15 +2,33 @@ class ClientVerification
   include DataMapper::Resource
   include Constants::Verification
 
-  property :id,                   Serial
-  property :verification_type,    Enum.send('[]', *CLIENT_VERIFICATION_TYPES), :nullable => false
-  property :verification_status,  Enum.send('[]', *CLIENT_VERIFICATION_STATUSES), :nullable => false, :default => NOT_VERIFIED
+  property :loan_application_id,  Integer, :nullable => false, :key => true, :index => true
+  property :verification_type,    Enum.send('[]', *CLIENT_VERIFICATION_TYPES), :nullable => false, :key => true, :index => true
+  property :verification_status,  Enum.send('[]', *CLIENT_VERIFICATION_STATUSES), :nullable => false, :default => NOT_VERIFIED, :key => true, :index => true
   property :verified_by_staff_id, Integer, :nullable => false
   property :verified_on_date,     Date, :nullable => false
   property :created_by_user_id,   Integer, :nullable => false
   property :created_at,           DateTime, :nullable => false, :default => DateTime.now
   
-  belongs_to :loan_application, :nullable => false
+  belongs_to :loan_application
+  
+  validates_with_method :one_status_only_per_CPV, :record_CPV2_only_when_CPV1_approved
+
+  # A CPV can only be approved or rejected
+  def one_status_only_per_CPV
+    statuses = ClientVerification.get_all_CPV_status(loan_application_id)
+    status_for_type = statuses[self.verification_type]
+    return [false, "Only one status can be recorded for #{self.verification_type} for #{self.loan_application_id}"] unless status_for_type == Constants::Verification::NOT_VERIFIED
+    true
+  end
+
+  # CPV2 is only applicable when CPV1 is approved
+  def record_CPV2_only_when_CPV1_approved
+    return true unless self.verification_type == Constants::Verification::CPV2
+    cpv1_is_verified = ClientVerification.is_CPV1_verified?(self.loan_application_id)
+    return [false, "CPV2 can only be recorded for a client that has been approved through CPV1"] unless cpv1_is_verified
+    true
+  end
 
   # Get all CPVs on the loan application
   def self.get_CPVs(for_loan_application)
@@ -60,6 +78,14 @@ class ClientVerification
   # Get CPV2 status on the loan application
   def self.get_CPV2_status(loan_application_id)
     get_CPV_status(loan_application_id, CPV2)
+  end
+
+  # Get the status of all CPVs performed on a loan application as a hash with the CPV names as keys
+  def self.get_all_CPV_status(loan_application_id)
+    statuses = {}
+    statuses[Constants::Verification::CPV1] = get_CPV1_status(loan_application_id)
+    statuses[Constants::Verification::CPV2] = get_CPV2_status(loan_application_id)
+    statuses
   end
 
   # Query for whether the loan application is CPV2 accepted
--- spec/models/client_verification_spec.rb
+++ spec/models/client_verification_spec.rb
@@ -2,20 +2,47 @@ require File.join( File.dirname(__FILE__), '..', "spec_helper" )
 
 describe ClientVerification do
 
-  before(:each) do
-    @cpv1 = ClientVerification.new
+  before(:all) do
+    @time_datum = Time.now
     @on_date = Date.today
-    @by_p_staff_id = 3; @by_q_staff_id = 7
-    @by_x_user_id = 29
+    @by_p_staff_id = 3;
+    @by_x_user_id = 29;
+    @counter = 1
   end
 
-  it "status should be set as expected" do
-    @cpv1.get_status.should == Constants::Verification::NOT_VERIFIED
-    @cpv1.set_status(Constants::Verification::VERIFIED_REJECTED, @on_date, @by_p_staff_id, @by_x_user_id)
-    @cpv1.get_status.should == Constants::Verification::VERIFIED_REJECTED
+  it "should record a CPV1 approved when requested" do
+    loan_application_id = ((Time.now - @time_datum) * 1000).to_i
+    ClientVerification.record_CPV1_approved(loan_application_id, @by_p_staff_id, @on_date, @by_x_user_id)
+    ClientVerification.is_CPV1_verified?(loan_application_id).should == true
+  end
 
-    @cpv1.set_status(Constants::Verification::VERIFIED_ACCEPTED, @on_date, @by_q_staff_id, @by_x_user_id)
-    @cpv1.get_status.should == Constants::Verification::VERIFIED_ACCEPTED
+  it "should record a CPV1 rejected when requested" do
+    loan_application_id = ((Time.now - @time_datum) * 1000).to_i
+    ClientVerification.record_CPV1_rejected(loan_application_id, @by_p_staff_id, @on_date, @by_x_user_id)
+    ClientVerification.is_CPV1_verified?(loan_application_id).should == false
+  end
+  
+  it "should not allow different statuses to be recorded for a CPV1 for a loan application" do
+    loan_application_id = ((Time.now - @time_datum) * 1000).to_i
+
+    ClientVerification.record_CPV1_rejected(loan_application_id, @by_p_staff_id, @on_date, @by_x_user_id)
+    ClientVerification.is_CPV1_verified?(loan_application_id).should == false
+
+    ClientVerification.record_CPV1_approved(loan_application_id, @by_p_staff_id, @on_date, @by_x_user_id)
+    ClientVerification.is_CPV1_verified?(loan_application_id).should == false
+  end
+
+  it "should disallow recording CPV2 on a loan application that has no CPV1" do
+    loan_application_id = ((Time.now - @time_datum) * 1000).to_i
+
+    ClientVerification.get_CPV1_status(loan_application_id).should == Constants::Verification::NOT_VERIFIED
+    ClientVerification.get_CPV2_status(loan_application_id).should == Constants::Verification::NOT_VERIFIED
+
+    ClientVerification.record_CPV2_approved(loan_application_id, @by_p_staff_id, @on_date, @by_x_user_id)
+    ClientVerification.get_CPV2_status(loan_application_id).should == Constants::Verification::NOT_VERIFIED
+
+    ClientVerification.record_CPV2_rejected(loan_application_id, @by_p_staff_id, @on_date, @by_x_user_id)
+    ClientVerification.get_CPV2_status(loan_application_id).should == Constants::Verification::NOT_VERIFIED
   end
 
 end
