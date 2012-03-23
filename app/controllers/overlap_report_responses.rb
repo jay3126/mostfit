193912289e66eff6f766d49e1af68122feaf7c52
Koustubh Sinkar <ksinkar@gmail.com>
2012-03-23 16:26:03 +0530

    Story 26336923: Upload Credit Bureau Response files
    1. Staff from the Operation team can (periodically) upload the Response
    files received from credit bureau.
    
    2. They will browse the filesystem and choose the file to be uploaded.

--- app/controllers/overlap_report_responses.rb
+++ app/controllers/overlap_report_responses.rb
@@ -3,5 +3,21 @@ class OverlapReportResponses < Application
   def index
     render
   end
-  
+ 
+  def upload_response
+    # this deals with an XML upload.
+    # since the XML upload changes the data in the system, it would be very useful to track all these changes
+    # and keep a copy of the XML file as well
+    if params[:file][:tempfile]
+      folder = File.join(Merb.root, "docs","highmark","responses")
+      filename = params[:file][:filename]
+      #filename = "highmark-response-#{DateTime.now.strftime('%Y-%m-%d-%H-%M-%s')}.xml"
+      FileUtils.mkdir_p(folder)
+      FileUtils.mv(params[:file][:tempfile].path, File.join(folder, filename))
+      
+      redirect url(:controller => :overlap_report_responses, :action => :index), :message => {:notice => "File uploaded succesfully"}
+      
+    end
+  end
+ 
 end
