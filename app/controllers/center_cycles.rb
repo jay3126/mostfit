6e21fa4cf0fb8ed49d604dc043f97309d6ef953b
kaushal kishor sharma <kaushal@kaushal-Vostro1310.(none)>
2012-04-04 12:17:03 +0530

    Center-Cycle Feature :- Second Commit

--- app/controllers/center_cycles.rb
+++ app/controllers/center_cycles.rb
@@ -8,30 +8,30 @@ class CenterCycles < Application
       :initiated_by_staff_id => center.manager.id, :created_at => Date.today, :cycle_number => 1,
       :status => 'open_center_cycle_status')
     if center_cycle.save
-      redirect resource(center.branch, center) ,:message => {:notice => "Save Succussfully"}
+      redirect resource(center.branch, center) ,:message => {:notice => "Save Successfully"}
     else
-      redirect resource(center.branch, center) ,:message => {:error => "Can not save..."}
+      redirect resource(center.branch, center) ,:message => {:error => "Cannot save..."}
     end
   end
 
   def update(id)
     center_cycle = CenterCycle.get params[:id]
     center = center_cycle.center
-    c_number = center_cycle.cycle_number + 1
     unless params[:closed_date].blank?
       if Date.today >= Date.parse(params[:closed_date])
-        center_cycle.update(:status => 'closed_center_cycle_status', :closed_on => params[:closed_date], :cycle_number => c_number, :closed_by_staff_id => center.manager.id)
+        center_cycle.update(:status => 'closed_center_cycle_status', :closed_on => params[:closed_date], :closed_by_staff_id => center.manager.id)
       else
-        error = "invild date"
+        error = "INVALID DATE"
       end
     else
+      c_number = center_cycle.cycle_number + 1
       center_cycle.update(:status => 'open_center_cycle_status', :closed_on => nil, :cycle_number => c_number, :closed_by_staff_id => nil, :initiated_on => params[:initiated_date])
     end
     
     if center_cycle.errors.blank? && error.blank?
-      redirect resource(center.branch, center), :message => {:notice => "Save Succussfully"}
+      redirect resource(center.branch, center), :message => {:notice => "Save Successfully"}
     else
-      redirect resource(center.branch, center), :message => {:error => "Can not update.."}
+      redirect resource(center.branch, center), :message => {:error => "Cannot update.."}
     end
   end
 
