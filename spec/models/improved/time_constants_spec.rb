809787becd99321f8ae20f03b9cf2510ba3ef6f0
kaushal <kaushal@kaushal-RC420-RC520-RC720.(none)>
2012-04-24 19:19:37 +0530

    Add more spec for Time methods

--- spec/models/improved/time_constants_spec.rb
+++ spec/models/improved/time_constants_spec.rb
@@ -37,21 +37,36 @@ describe Constants::Time do
 
   it "should be return Array of Dates of week" do
     on_date = Date.parse('2012-04-23')
-    week_days = Constants::Time::get_current_week_dates(on_date)
-    week_days.should be_an_instance_of Array
-    week_days.each { |date|
+    on_date_sun = Date.parse('2012-04-22')
+    on_date_mon = Date.parse('2012-04-23')
+    on_date_tue = Date.parse('2012-04-24')
+    on_date_wed = Date.parse('2012-04-25')
+    on_date_thu = Date.parse('2012-04-26')
+    on_date_fri = Date.parse('2012-04-27')
+    on_date_sat = Date.parse('2012-04-28')
+    
+    week_dates = Constants::Time::get_current_week_dates(on_date)
+    week_dates.should be_an_instance_of Array
+    week_dates.each { |date|
       date.should be_an_instance_of Date
     }
-    week_days.size.should == 7
-    # Suggestion: Also check the date!!! And rename the variable to reflect dates
-    # Remove this suggestion comment once implemented
-    # Also, use the declared constants for weekdays, etc.
-    week_days[0].weekday.should == :sunday
-    week_days[1].weekday.should == :monday
-    week_days[2].weekday.should == :tuesday
-    week_days[3].weekday.should == :wednesday
-    week_days[4].weekday.should == :thursday
-    week_days[5].weekday.should == :friday
-    week_days[6].weekday.should == :saturday
+    week_dates.size.should == 7
+    
+    week_dates[0].weekday.should == Constants::Time::SUNDAY
+    week_dates[1].weekday.should == Constants::Time::MONDAY
+    week_dates[2].weekday.should == Constants::Time::TUESDAY
+    week_dates[3].weekday.should == Constants::Time::WEDNESDAY
+    week_dates[4].weekday.should == Constants::Time::THURSDAY
+    week_dates[5].weekday.should == Constants::Time::FRIDAY
+    week_dates[6].weekday.should == Constants::Time::SATURDAY
+
+    week_dates[0].should == on_date_sun
+    week_dates[1].should == on_date_mon
+    week_dates[2].should == on_date_tue
+    week_dates[3].should == on_date_wed
+    week_dates[4].should == on_date_thu
+    week_dates[5].should == on_date_fri
+    week_dates[6].should == on_date_sat
+
   end
 end
