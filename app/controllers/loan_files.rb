793a4bd326fc6a75bc0a1e375108afc19bf75c15
kirti <kirti@kirti-Lenovo-G570.(none)>
2012-04-30 18:12:59 +0530

    Bug #2336: proper validation on 'staff member id' if it is blank.RESOLVED

--- app/controllers/loan_files.rb
+++ app/controllers/loan_files.rb
@@ -12,7 +12,6 @@ class LoanFiles < Application
   end
   
   def get_data(params)
-    @errors = {}
     if params[:branch_id] && params[:branch_id].empty?
       @errors["Loan File"] = "No branch selected"
     elsif params[:center_id] && params[:center_id].empty?
@@ -43,47 +42,37 @@ class LoanFiles < Application
   end 
 
   def pending_loan_file_generation
+    @errors = {}
     get_data(params)
     render :loan_file_generation
   end
 
   def record_loan_file
-    @errors = {}
-
+    @errors = []
+    laf = LoanApplicationsFacade.new(session.user)
+    created_on = params['created_on']
+    scheduled_disbursal_date = params['scheduled_disbursal_date']
+    scheduled_first_payment_date = params['scheduled_first_payment_date']
+    created_by_staff_id = params['created_by_staff_id']
+    @errors << "No staff member selected " if created_by_staff_id.blank?
+    @errors << "No loan application selected" unless params.key?('selected')
     #record the loan file first
     #was any data actually sent ?
-    if params.key?('selected')
+    if @errors.blank?
       @loan_applications = params['selected'].keys
       @branch_id = params['branch_id'] ? params['branch_id'] : nil
       @center_id = params['center_id'] ? params['center_id'] : nil
       @for_cycle_number = 1
-
-      laf = LoanApplicationsFacade.new(session.user)
-      created_by_staff_id = params['created_by_staff_id']
-      if created_by_staff_id.nil?
-        @errors['Loan File Generation'] = "No staff member selected! "
-        get_data(params)
-        render :loan_file_generation
-      end
-
-      created_on = params['created_on']
-      scheduled_disbursal_date = params['scheduled_disbursal_date']
-      scheduled_first_payment_date = params['scheduled_first_payment_date']
       if params['loan_file_identifier'] and not params['loan_file_identifier'].nil?
-        @loan_file = laf.locate_loan_file(params['loan_file_identifier']) 
+        @loan_file = laf.locate_loan_file(params['loan_file_identifier'])
         laf.add_to_loan_file(@loan_file.loan_file_identifier, @branch_id, @center_id, @for_cycle_number, created_by_staff_id, created_on, *@loan_applications)
       else
-        @loan_file = laf.create_loan_file(@branch_id, @center_id, @for_cycle_number, 
+        @loan_file = laf.create_loan_file(@branch_id, @center_id, @for_cycle_number,
           scheduled_disbursal_date, scheduled_first_payment_date,
           created_by_staff_id, created_on, *@loan_applications)
       end
-    else
-      @errors['Loan File Generation'] = "No loan applications selected!"
     end
     get_data(params)
-    if @errors.empty?
-      @show_recorded = true
-    end
 
     render :loan_file_generation
   end
