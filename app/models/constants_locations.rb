9b5434ff2c24bf28ed1b855d47b657a2594d71f5
Krishnan Mani <km@krishnanm.com>
2012-05-11 12:20:41 +0530

    Also checking in Constants::Locations required by Resolver. This will be deprecated soon.

---
+++ app/models/constants_locations.rb
@@ -0,0 +1,11 @@
+module Constants
+  module Locations
+
+    CENTER = :center
+    MODELS_AND_LOCATIONS = { "Center" => CENTER }
+    LOCATIONS = [CENTER]
+
+    LOCATION_LEVELS = [1, 2, 3, 4]
+
+  end
+end
