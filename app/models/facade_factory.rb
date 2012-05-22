24a4a17652160eea86253d0ece2c0c899af2a72e
Krishnan Mani <km@krishnanm.com>
2012-05-22 19:22:03 +0530

    UserFacade additions to query staff assignments for designation on a given date

--- app/models/facade_factory.rb
+++ app/models/facade_factory.rb
@@ -5,11 +5,14 @@ class FacadeFactory
 
   attr :created_at
 
-  MEETING_FACADE = :meeting
-  LOAN_FACADE = :loan
+  MEETING_FACADE  = :meeting
+  LOAN_FACADE     = :loan
   LOCATION_FACADE = :location
-  PAYMENT_FACADE = :payment
-  FACADE_TYPES = { MEETING_FACADE => MeetingFacade, LOAN_FACADE => LoanFacade, LOCATION_FACADE => LocationFacade, PAYMENT_FACADE => PaymentFacade }
+  PAYMENT_FACADE  = :payment
+  USER_FACADE     = :user
+  FACADE_TYPES    = { MEETING_FACADE  => MeetingFacade, LOAN_FACADE => LoanFacade,
+                      LOCATION_FACADE => LocationFacade, PAYMENT_FACADE => PaymentFacade,
+                      USER_FACADE     => UserFacade }
 
   def initialize
     @created_at = DateTime.now
@@ -17,14 +20,14 @@ class FacadeFactory
 
   # Factory that locates and returns instances of the requested facade that is_a
   # StandardFacade
-  def get_instance(of_facade_type, for_user, with_options = {})
+  def get_instance(of_facade_type, for_user, with_options = { })
     facade_klass = FACADE_TYPES[of_facade_type]
     raise ArgumentError, "No facade configured for requested facade type: #{of_facade_type}" unless facade_klass
     facade_klass.new(for_user, with_options)
   end
 
   # Factory that returns an instance of a facade to be invoked from another facade
-  def get_other_facade(of_facade_type, given_facade, with_options = {})
+  def get_other_facade(of_facade_type, given_facade, with_options = { })
     for_user = given_facade.for_user
     get_instance(of_facade_type, for_user, with_options)
   end
---
+++ app/models/user_facade.rb
@@ -0,0 +1,35 @@
+class UserFacade <StandardFacade
+
+  ###########
+  # QUERIES # on user begin
+  ###########
+
+  def get_user(for_login)
+    #TODO
+  end
+
+  def get_first_user
+    #TODO
+  end
+
+  def get_operator
+    #TODO
+  end
+
+  ###########
+  # QUERIES # on user end
+  ###########
+
+  def get_designation(for_staff_id, on_date)
+    StaffAssignment.get_designation(for_staff_id, on_date)
+  end
+
+  ###########
+  # UPDATES #
+  ###########
+
+  def assign_designation(designation, to_staff, performed_by, performed_at, effective_on = Date.today)
+    StaffAssignment.assign(designation, to_staff, performed_by, performed_at, effective_on)
+  end
+
+end
