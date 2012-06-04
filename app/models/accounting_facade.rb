bfe3813553a0d3d479b3e5c17af27046fd6dca3d
Krishnan Mani <km@krishnanm.com>
2012-06-04 13:01:16 +0530

    Corrections to accounting facade

--- app/models/accounting_facade.rb
+++ app/models/accounting_facade.rb
@@ -1,23 +1,23 @@
 class AccountingFacade < StandardFacade
 
   def get_primary_chart_of_accounts
-    @book_keeper.get_primary_chart_of_accounts
+    book_keeper.get_primary_chart_of_accounts
   end
 
   def get_ledger(by_ledger_id)
-    @book_keeper.get_ledger(by_ledger_id)
+    book_keeper.get_ledger(by_ledger_id)
   end
 
   def get_ledger_opening_balance_and_date(by_ledger_id)
-    @book_keeper.get_ledger_opening_balance_and_date(by_ledger_id)
+    book_keeper.get_ledger_opening_balance_and_date(by_ledger_id)
   end
 
   def get_current_ledger_balance(by_ledger_id)
-    @book_keeper.get_ledger_current_balance(by_ledger_id)
+    book_keeper.get_ledger_current_balance(by_ledger_id)
   end
 
   def get_historical_ledger_balance(by_ledger_id, on_date)
-    @book_keeper.get_historical_ledger_balance(by_ledger_id, on_date)
+    book_keeper.get_historical_ledger_balance(by_ledger_id, on_date)
   end
 
   def get_vouchers(involving_ledger_id, on_or_before_date)
@@ -26,7 +26,7 @@ class AccountingFacade < StandardFacade
 
   private
 
-  def get_book_keeper
+  def book_keeper
     @book_keeper ||= MyBookKeeper.new
   end
 
