1ad4fc0240b1cc1dc7877e3f5b00d8b1959bfd1f
kaushal <kaushal@kaushal-RC420-RC520-RC720.(none)>
2012-06-08 01:57:23 +0530

    create approve and disburse loan view pages

--- app/controllers/lendings.rb
+++ app/controllers/lendings.rb
@@ -1,12 +1,10 @@
 class Lendings < Application
 
   def index
-    @lendings = Lending.all
-    if @lendings.blank?
-      redirect resource(:lending_products)
-    else
-      display @lendings
-    end
+    @new_lendings = Lending.all(:status => [:new_loan_status])
+    @approve_lendings = Lending.all(:status => [:approved_loan_status])
+    @disburse_lendings = Lending.all(:status => [:disbursed_loan_status])
+    display @new_lendings
   end
 
   def new
@@ -117,4 +115,73 @@ class Lendings < Application
 
     redirect resource(:lendings) , :message => @message
   end
+
+  def update_lending_approve_to_disburse
+    @message = {}
+    lendings = []
+    lending_params = params[:lending]
+    begin
+      lending_params.each do |key, value|
+        if value.size == 2
+          lending = Lending.get key
+          disbursed_amount = MoneyManager.get_money_instance(value.last[:disbursed_amount])
+          lending.disbursed_amount = disbursed_amount.amount
+          lending.disbursed_by_staff = value.last[:disbursed_by_staff]
+          if lending.valid?
+            lendings << lending
+          else
+            @message = {:error => "#{lending.id}= #{lending.errors.first.join(', ')}"}
+          end
+        end
+      end
+      if @message[:error].blank?
+        mf = FacadeFactory.instance.get_instance(FacadeFactory::PAYMENT_FACADE, session.user.id)
+        lendings.each do |lending|
+          payment_transaction = mf.record_payment(lending.to_money[:disbursed_amount], 'payment', 'lending', lending.id, 'client', lending.for_borrower_id, lending.administered_at_origin, lending.accounted_at_origin, lending.disbursed_by_staff, Date.today, nil)
+          if lending.disburse(payment_transaction)
+            @message = {:notice => "Loans disbursed successfully."}
+          else
+            @message = {:error => "Loans desbursed fails."}
+          end
+        end
+      end
+    rescue => ex
+      @message = {:error => "An error has occured: #{ex.message}"}
+    end
+
+    redirect resource(:lendings) , :message => @message
+  end
+
+  def payment_on_disburse_loan
+    @message = {}
+    payments = []
+    lending_params = params[:lending]
+    begin
+      lending_params.each do |key, value|
+        if value.size == 2
+          lending = Lending.get key
+          payment_amount = MoneyManager.get_money_instance(value.last[:payment_amount])
+          payment_by_staff = value.last[:payment_by_staff]
+          payments << {:lending => lending, :payment_amount => payment_amount, :payment_by_staff => payment_by_staff}
+        end
+      end
+      if @message[:error].blank?
+        mf = FacadeFactory.instance.get_instance(FacadeFactory::PAYMENT_FACADE, session.user.id)
+        payments.each do |payment|
+          lending = payment[:lending]
+          payment_transaction = mf.record_payment(payment[:payment_amount], 'receipt', 'lending', lending.id, 'client', lending.for_borrower_id, lending.administered_at_origin, lending.accounted_at_origin, payment[:payment_by_staff], Date.today, nil)
+          if payment_transaction.new?
+            @message = {:error => "Loans payments fails."}
+          else
+            @message = {:notice => "Loans payments successfully."}
+          end
+        end
+      end
+    rescue => ex
+      @message = {:error => "An error has occured: #{ex.message}"}
+    end
+
+    redirect resource(:lendings) , :message => @message
+  end
+
 end
