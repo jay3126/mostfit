ebaeea1534ae53d9eb245cffec95bb9be5d945b5
Kirti Parihar <pariharkirti24@gmail.com>
2012-06-08 20:18:18 +0530

    Fixed crash on tranches.

--- app/controllers/tranches.rb
+++ app/controllers/tranches.rb
@@ -5,26 +5,26 @@ class Tranches < Application
   def list(id)
 
     # GATE-KEEPING
-      funding_line_id=id
+    funding_line_id=id
     
     # VALIDATION
-      if FundingLine.get(funding_line_id).nil?
-	redirect("/funders", :message => {:notice => "Funding line not found"})	
-      end
+    if FundingLine.get(funding_line_id).nil?
+      redirect("/funders", :message => {:notice => "Funding line not found"})
+    end
     # OPERATIONS PERFORMED
     
-      begin
-        @funding_line=FundingLine.get(id)
-        @tranches=@funding_line.tranches(:order => [ :date_of_commencement .asc ])
-        #@tranches.sort! { |a,b| a.date_of_commencement <=> b.date_of_commencement }
-      rescue => ex
-         #@errors.push(ex.message)
-      end
+    begin
+      @funding_line=FundingLine.get(id)
+      @tranches=@funding_line.tranches(:order => [ :date_of_commencement .asc ])
+      #@tranches.sort! { |a,b| a.date_of_commencement <=> b.date_of_commencement }
+    rescue => ex
+      #@errors.push(ex.message)
+    end
 
     # POPULATING RESPONSE AND OTHER VARIABLES
     
     # RENDER/RE-DIRECT
-       display [@tranches,@funding_line]
+    display [@tranches,@funding_line]
   end	
 
   def index
@@ -33,43 +33,50 @@ class Tranches < Application
   
   def new(id)
     # GATE-KEEPING
-      funding_line_id=id
+    funding_line_id=id
     
     # VALIDATION
-      if FundingLine.get(funding_line_id).nil?
-	redirect(params[:return], :message => {:notice => "Funding line not found"})	
-      end
+    if FundingLine.get(funding_line_id).nil?
+      redirect(params[:return], :message => {:notice => "Funding line not found"})
+    end
     # OPERATIONS PERFORMED
     
-      begin
-        @tranch=Tranch.new
-        @tranch.funding_line_id=id
-      rescue => ex
-         @errors.push(ex.message)
-      end
+    begin
+      @tranch=Tranch.new
+      @tranch.funding_line_id=id
+    rescue => ex
+      @errors.push(ex.message)
+    end
 
     # POPULATING RESPONSE AND OTHER VARIABLES
     
     # RENDER/RE-DIRECT
-      display @tranch  
+    display @tranch
   end
   
   def create(tranch)
     # GATE-KEEPING
-      @tranch = Tranch.new(tranch)
-    
-    # VALIDATION
+    @errors = []
+    @tranch = Tranch.new(tranch)
+    @errors << "Tranch name must not be blank " if params[:tranch][:name].blank?
+    @errors << "Date must not be blank " if params[:tranch][:date_of_commencement].blank?
+    if @errors.empty?
+      # VALIDATION
       if(Tranch.all(:name => @tranch.name,:funding_line=>@tranch.funding_line).count!=0)
         message[:error] = "Tranch with same name already exists !"
         render :new  # error messages will show
-     else
-    # OPERATIONS PERFORMED
-      if @tranch.save!
-        redirect("/tranches/list/#{@tranch.funding_line_id}", :message => {:notice => "Tranch '#{@tranch.name}' (Id:#{@tranch.id}) successfully created"})
       else
-        message[:error] = "Tranch failed to be created"
-        render :new  # error messages will show
+        # OPERATIONS PERFORMED
+        if @tranch.save!
+          redirect("/tranches/list/#{@tranch.funding_line_id}", :message => {:notice => "Tranch '#{@tranch.name}' (Id:#{@tranch.id}) successfully created"})
+        else
+          message[:error] = "Tranch failed to be created"
+          render :new  # error messages will show
+        end
       end
+    else
+      message[:error] = @errors.to_s
+      render :new  # error messages will show
     end
 
     # POPULATING RESPONSE AND OTHER VARIABLES
