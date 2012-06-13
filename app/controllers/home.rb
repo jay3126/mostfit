6f698d9298b11b331bc263aa7081d40c30a62a14
jay3126 <janmejay.rai@intellecap.com>
2012-06-13 19:43:18 +0530

    bugs and redirection issue of effective date fixed
    Conflicts:
    
    	slices/merb-auth-slice-password/app/controllers/sessions.rb

--- app/controllers/home.rb
+++ app/controllers/home.rb
@@ -4,4 +4,21 @@ class Home < Application
     @location_levels = LocationLevel.all
     display @location_levels
   end
+
+  def effective_date
+    @date = get_effective_date
+    display @date
+  end
+
+  def update_effective_date
+    message = {}
+    date = Date.parse(params[:effective_date])
+    if set_effective_date(date)
+      message[:notice] = "Effective date update successfully"
+    else
+      message[:error] = "Effective date cannot updated"
+    end
+    redirect url(:controller => :home, :action => 'index'), :message => message
+  end
+
 end
