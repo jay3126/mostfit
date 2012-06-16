0502260128cf1a24cc12a1563ce3a675e54c9bc5
jay3126 <janmejay.rai@intellecap.com>
2012-06-16 12:34:42 +0530

    New daily report now shows correct figures for Loan amounts filtered either by one biz_location or selecting all.
    Payment amounts to follow next.

--- app/models/reports/new_daily_report.rb
+++ app/models/reports/new_daily_report.rb
@@ -4,6 +4,9 @@ class NewDailyReport < Report
   def initialize(params, dates, user)
     @date = dates[:date] || Date.today
     @name = "Daily Report for #{@date}"
+    location_facade = FacadeFactory.instance.get_instance(FacadeFactory::LOCATION_FACADE, @user)
+    branches = location_facade.all_nominal_branches
+    @biz_location_branch = (params and params[:biz_location_branch] and (not (params[:biz_location_branch].empty?))) ? params[:biz_location_branch] : branches
     get_parameters(params, user)
   end
 
@@ -15,19 +18,103 @@ class NewDailyReport < Report
     "New Daily Report"
   end
 
+  def default_currency
+    @default_currency = MoneyManager.get_default_currency
+  end
+
   def generate
+    data , loan_applied_amount , loan_approved_amount , loan_disbursed_amount = {}, {}, {}, {}
+    loan_collections = []
 
-    data = {}
-
-    #finding out a list of loan_ids at a particular biz_location and on a particular date
     loan_facade = FacadeFactory.instance.get_instance(FacadeFactory::LOAN_FACADE, @user)
-    loan_ids = loan_facade.get_loans_at_location(@biz_location_branch, @date)
+    #handling condition when only one choice is selected. It gives the id of the bizlocation so checking it with the class of biz_location.
+    if (@biz_location_branch and @biz_location_branch.class == Fixnum) 
+      biz_location = BizLocation.get(@biz_location_branch)
+      loan = loan_facade.get_loans_at_location(biz_location.id, @date)
+      loan_ids = loan.map{|l| l.id}
 
-    #loan amounts (applied, approved and disbursed)
-    loan_applied_amount = Lending.all(:id => loan_ids, :applied_on_date => @date).aggregate(:applied_amount.sum)
-    loan_approved_amount = Lending.all(:id => loan_ids, :approved_on_date => @date).aggregate(:approved_amount.sum)
-    loan_disbursed_amount = Lending.all(:id => loan_ids, :disbursal_date => @date).aggregate(:disbursed_amount.sum)
+      #handling cases if loan_ids are empty.
+      if loan_ids.empty?
+        loan_applied_amount[biz_location.name] = 0
+        loan_approved_amount[biz_location.name] = 0
+        loan_disbursed_amount[biz_location.name] = 0
 
+      else
+        applied_amount = Lending.all(:id => loan_ids, :applied_on_date => @date).aggregate(:applied_amount.sum)
+        if applied_amount.nil?
+          loan_applied_amount[biz_location.name] = 0
+        else
+          loan_applied_amount[biz_location.name] = applied_amount
+        end
+
+        approved_amount = Lending.all(:id => loan_ids, :approved_on_date => @date).aggregate(:approved_amount.sum)
+        if approved_amount.nil?
+          loan_approved_amount[biz_location.name] = 0
+        else
+          loan_approved_amount[biz_location.name] = approved_amount
+        end
+
+        disbursed_amount = Lending.all(:id => loan_ids, :disbursal_date => @date).aggregate(:disbursed_amount.sum)
+        if disbursed_amount.nil?
+          loan_disbursed_amount[biz_location.name] = 0
+        else
+          loan_disbursed_amount[biz_location.name] = disbursed_amount
+        end
+      end
+
+    else
+      #when no choice is selected, we get a collection of all the biz_locations.
+      @biz_location_branch.each do |biz_location|
+        loans = loan_facade.get_loans_at_location(biz_location.id, @date)
+        loan_ids = loans.map{|l| l.id}
+
+        if loan_ids.empty?
+          loan_applied_amount[biz_location.name] = 0
+          loan_approved_amount[biz_location.name] = 0
+          loan_disbursed_amount[biz_location.name] = 0
+
+        else
+          applied_amount = Lending.all(:id => loan_ids, :applied_on_date => @date).aggregate(:applied_amount.sum)
+          if applied_amount.nil?
+            loan_applied_amount[biz_location.name] = 0
+          else
+            loan_applied_amount[biz_location.name] = applied_amount
+          end
+
+          approved_amount = Lending.all(:id => loan_ids, :approved_on_date => @date).aggregate(:approved_amount.sum)
+          if approved_amount.nil?
+            loan_approved_amount[biz_location.name] = 0
+          else
+            loan_approved_amount[biz_location.name] = approved_amount
+          end
+
+          disbursed_amount = Lending.all(:id => loan_ids, :disbursal_date => @date).aggregate(:disbursed_amount.sum)
+          if disbursed_amount.nil?
+            loan_disbursed_amount[biz_location.name] = 0
+          else
+            loan_disbursed_amount[biz_location.name] = disbursed_amount
+          end
+        end
+
+      end
+    end
+
+    #loop for sending the data to view side.
+    if (@biz_location_branch and @biz_location_branch.class == Fixnum)
+      biz_location = BizLocation.get(@biz_location_branch)
+      loan_amounts_map = {:amount_applied => loan_applied_amount.values[0], :amount_approved => loan_approved_amount.values[0], :amount_disbursed => loan_disbursed_amount.values[0]}
+      loan_money_amounts_map = Money.money_amounts_hash_to_money(loan_amounts_map, default_currency)
+      data[biz_location.name] = {:loans => loan_money_amounts_map }
+
+    else
+      @biz_location_branch.each do |biz_location|
+        loan_amounts_map = {:amount_applied => loan_applied_amount[biz_location.name], :amount_approved => loan_approved_amount[biz_location.name], :amount_disbursed => loan_disbursed_amount[biz_location.name]}
+        loan_money_amounts_map = Money.money_amounts_hash_to_money(loan_amounts_map, default_currency)
+        data[biz_location.name] = {:loans => loan_money_amounts_map }
+      end
+    end
+
+    return data
     
 =begin
     The report will be filtered with branch as of now.
