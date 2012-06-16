fb2842fdf0be5b13e02db7bc7196f0774cc932c9
Kirti Parihar <pariharkirti24@gmail.com>
2012-06-16 10:21:07 +0530

    Integrated bizlocation for loan application report (jquery needs to handle also impacts on loan file report, needs to fix)

--- app/models/reports/loan_applications_report.rb
+++ app/models/reports/loan_applications_report.rb
@@ -1,12 +1,12 @@
 class LoanApplicationsReport < Report
-  attr_accessor :branch, :center, :branch_id, :center_id, :loan_application_status
+  attr_accessor :branch, :center, :branch_for_loan_application_report, :center_for_loan_application_report, :loan_application_status, :effective_date
 
   def initialize(params, date, user)
     @date = date.blank? ? Date.today : date
     @name   = "Report on #{@date}"
     @loan_application_status = params[:loan_application_status].gsub(' ',"_").downcase rescue ""
-    @center_id = params[:center_id] rescue ""
-    @branch_id = params[:branch_id] rescue ""
+    @center_id = params[:branch_for_loan_application_report] rescue ""
+    @branch_id = params[:center_for_loan_application_report] rescue ""
     get_parameters(params, user)
   end
 
@@ -25,9 +25,9 @@ class LoanApplicationsReport < Report
     condition_hash.merge!(:at_center_id => @center_id) unless @center_id.blank?
     condition_hash.merge!(:at_branch_id => @branch_id) unless @branch_id.blank?
     loan_applications = LoanApplication.all(condition_hash)
-    branches = Branch.all(:id => loan_applications.map(&:at_branch_id))
-    centers = Center.all(:id => loan_applications.map(&:at_center_id))
-    loan_applications.group_by{|x| [x.at_branch_id,x.at_center_id,x.status]}.collect{|c| loan_app.merge!(branches.find{|x| x.id == c[0][0]}.name => { centers.find{|x| x.id == c[0][1]}.name => { c[0][2] => c[1].count}})}
+    branches = BizLocation.all(:id => loan_applications.map(&:at_branch_id))
+    centers = BizLocation.all(:id => loan_applications.map(&:at_center_id))
+    loan_applications.group_by{|x| [x.at_branch_id,x.at_center_id,x.status]}.collect{|c| loan_app.merge!(branches.find{|x| x.id == c[0][0]}.id => { centers.find{|x| x.id == c[0][1]}.id => { c[0][2] => c[1].count}})}
     loan_app
   end
   
