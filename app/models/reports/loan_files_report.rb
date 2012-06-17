69ea8c21c427a7300d85054eefb15d5ab844cee0
Kirti Parihar <pariharkirti24@gmail.com>
2012-06-17 11:49:39 +0530

    BizLocation integration for loan file report

--- app/models/reports/loan_files_report.rb
+++ app/models/reports/loan_files_report.rb
@@ -1,12 +1,12 @@
 class LoanFilesReport < Report
-  attr_accessor :branch, :center, :branch_id, :center_id, :loan_file_status
+  attr_accessor :branch, :center, :branch_for_loan_application_report, :center_for_loan_application_report, :loan_file_status, :effective_date
 
   def initialize(params, date, user)
     @date = date.blank? ? Date.today : date
     @name   = "Report on #{@date}"
     @loan_file_status = params[:loan_file_status].gsub(' ',"_").downcase rescue ""
-    @center_id = params[:center_id] rescue ""
-    @branch_id = params[:branch_id] rescue ""
+    @branch_id = params[:branch_for_loan_application_report] rescue ""
+    @center_id = params[:center_for_loan_application_report] rescue ""
     get_parameters(params, user)
   end
 
@@ -25,9 +25,9 @@ class LoanFilesReport < Report
     condition_hash.merge!(:at_center_id => @center_id) unless @center_id.blank?
     condition_hash.merge!(:at_branch_id => @branch_id) unless @branch_id.blank?
     loan_files = LoanFile.all(condition_hash)
-    branches = Branch.all(:id => loan_files.map(&:at_branch_id))
-    centers = Center.all(:id => loan_files.map(&:at_center_id))
-    loan_files.group_by{|x| [x.at_branch_id,x.at_center_id,x.health_check_status]}.collect{|c| loan_app.merge!(branches.find{|x| x.id == c[0][0]}.name => { centers.find{|x| x.id == c[0][1]}.name => { c[0][2] => c[1].count}})}
+    branches = BizLocation.all(:id => loan_files.map(&:at_branch_id))
+    centers = BizLocation.all(:id => loan_files.map(&:at_center_id))
+    loan_files.group_by{|x| [x.at_branch_id,x.at_center_id,x.health_check_status]}.collect{|c| loan_app.merge!(branches.find{|x| x.id == c[0][0]}.id => { centers.find{|x| x.id == c[0][1]}.id => { c[0][2] => c[1].count}})}
     loan_app
   end
   
