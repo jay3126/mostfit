cfd358cc4622b5aa4d312c99cef288cdf21eec41
kaushal <kaushal@kaushal-RC420-RC520-RC720.(none)>
2012-06-20 16:25:33 +0530

    create daily collection Pdf for staff member

--- lib/pdfs/day_sheet.rb
+++ lib/pdfs/day_sheet.rb
@@ -5,108 +5,105 @@ module Pdf
       folder   = File.join(Merb.root, "doc", "pdfs", "staff", self.name, "collection_sheets")
       FileUtils.mkdir_p(folder)
       filename = File.join(folder, "collection_#{self.id}_#{date.strftime('%Y_%m_%d')}.pdf")
-      center_ids = LoanHistory.all(:date => [date, date.holidays_shifted_today].uniq, :fields => [:loan_id, :date, :center_id], :status => [:disbursed, :outstanding]).map{|x| x.center_id}.uniq
-      centers = self.centers(:id => center_ids).sort_by{|x| x.name}
+      # center_ids = LoanHistory.all(:date => [date, date.holidays_shifted_today].uniq, :fields => [:loan_id, :date, :center_id], :status => [:disbursed, :outstanding]).map{|x| x.center_id}.uniq
+      # centers = self.centers(:id => center_ids).sort_by{|x| x.name}
+      @biz_locations = LocationManagement.get_locations_for_staff(self, date)
       pdf = PDF::Writer.new(:orientation => :landscape, :paper => "A4")
       pdf.select_font "Times-Roman"
       pdf.text "Daily Collection Sheet for #{self.name} for #{date}", :font_size => 24, :justification => :center
       pdf.text("\n")
-      return nil if centers.empty?
-      days_absent = Attendance.all(:status => "absent", :center => centers).aggregate(:client_id, :all.count).to_hash
-      days_present = Attendance.all(:center => centers).aggregate(:client_id, :all.count).to_hash
+      #return nil if centers.empty?
+      return nil if @biz_locations.empty?
+      #days_absent = Attendance.all(:status => "absent", :center => centers).aggregate(:client_id, :all.count).to_hash
+      #days_present = Attendance.all(:center => centers).aggregate(:client_id, :all.count).to_hash
       weeksheets = CollectionsFacade.new(self.id).get_collection_sheet_for_staff(date)
       idx = 0
       weeksheets.each do |weeksheet|
-        center = Center.get weeksheet.at_center_id
-        pdf.start_new_page if idx > 0
-        pdf.text "Center: #{center.name}, Manager: #{self.name}, signature: ______________________", :font_size => 12, :justification => :left
-        pdf.text("Center leader: #{center.leader.client.name}, signature: ______________________", :font_size => 12, :justification => :left) if center.leader
-        pdf.text("Date: #{date}, Time: #{center.meeting_time_hours}:#{'%02d' % center.meeting_time_minutes}", :font_size => 12, :justification => :left)
-        pdf.text("\n")
+        unless weeksheet.blank?
+          location = BizLocation.get weeksheet.at_biz_location_id
+          pdf.start_new_page if idx > 0
+          pdf.text "Location: #{location.name}, Manager: #{self.name}, signature: ______________________", :font_size => 12, :justification => :left
+          #pdf.text("Center leader: #{location.leader.client.name}, signature: ______________________", :font_size => 12, :justification => :left) if location.leader
+          pdf.text("Date: #{date}, Time: #{weeksheet.at_meeting_time_begins_hours}:#{'%02d' % weeksheet.at_meeting_time_begins_minutes}", :font_size => 12, :justification => :left)
+          pdf.text("Meeting Status: No Meeting", :font_size => 12, :justification => :left) unless MeetingCalendar.meeting_at_location_on_date(location, date)
+          pdf.text("\n")
 
-        table = PDF::SimpleTable.new
-        table.data = []
-        tot_amount, tot_outstanding, tot_installments, tot_principal, tot_interest, total_due, tot_fee= 0, 0, 0, 0, 0, 0, 0
-        weeksheet.groups.each do |group|
-          collection_sheets = weeksheet.collection_sheet_lines.select{|cs| cs.borrower_group_id == group[0]}.sort_by{|cs| cs.borrower_name} rescue []
-          group_amount, group_outstanding, group_installments, group_principal, group_interest, group_fee, group_due = 0, 0, 0, 0, 0, 0, 0
-          table.data.push({"disbursed" => group ? group[1] : "No group"})
-          loan_row_count=0
-          collection_sheets.each do |ws|
-            table.data.push({"name" => ws.borrower_name, "loan id" => ws.loan_id, "amount" => ws.loan_disbursed_amount.to_currency,
-                "outstanding" => ws.loan_outstanding.to_currency, "status" => ws.loan_status,
-                "disbursed" => ws.loan_disbursed_date, "installment" => ws.loan_installment_number,
-                "principal" => ws.principal_due_today.to_currency, "interest" => ws.interest_due_today.to_currency,
-                "days absent/total" => (days_absent[ws.borrower_id]||0).to_s / (days_present[ws.borrower_id]||0).to_s,
-                "fee" => ws.fee_due_today.to_currency, "total due" =>  ws.total_due.to_currency, "signature" => "" })
-            group_amount       += ws.loan_disbursed_amount
-            group_outstanding  += ws.loan_outstanding
-            group_installments += ws.loan_installment_number
-            group_principal    += ws.principal_due_today
-            group_interest     += ws.interest_due_today
-            group_fee          += ws.fee_due_today
-            group_due          += ws.total_due
-            loan_row_count     += 1
-            if loan_row_count==0
-              table.data.push({"name" => ws.borrower_name, "signature" => "", "status" => "nothing outstanding"})
-            end
-          end
-          table.data.push({"amount" => group_amount.to_currency, "outstanding" => group_outstanding.to_currency,
-              "principal" => group_principal.to_currency, "interest" => group_interest.to_currency,
-              "fee" => group_fee.to_currency, "total due" => group_due.to_currency
-            })
-          tot_amount         += group_amount
-          tot_outstanding    += group_outstanding
-          tot_installments   += group_installments
-          tot_principal      += group_principal
-          tot_interest       += group_interest
-          tot_fee            += group_fee
-          total_due          += (group_principal + group_interest + group_fee)
-
-        end
-        table.data.push({"amount" => tot_amount.to_currency, "outstanding" => tot_outstanding.to_currency,
-            "principal" => tot_principal.to_currency,
-            "interest" => tot_interest.to_currency, "fee" => tot_fee.to_currency,
-            "total due" => (tot_principal + tot_interest + tot_fee).to_currency
-          })
-        table.column_order  = ["name", "loan id" , "amount", "outstanding", "status", "disbursed", "installment", "principal", "interest", "fee", "total due", "days absent/total", "signature"]
-        table.show_lines    = :all
-        table.show_headings = true
-        table.shade_rows    = :none
-        table.shade_headings = true
-        table.orientation   = :center
-        table.position      = :center
-        table.title_font_size = 16
-        table.header_gap = 10
-        table.render_on(pdf)
-
-        #draw table for scheduled disbursals
-        loans_to_disburse = center.clients.loans(:scheduled_disbursal_date => date)
-        if center.clients.count>0 and loans_to_disburse.count > 0
           table = PDF::SimpleTable.new
           table.data = []
+          tot_amount, tot_outstanding, tot_installments, tot_principal, tot_interest, total_due, tot_fee= 0, 0, 0, 0, 0, 0, 0
+          weeksheet.groups.each do |group|
+            collection_sheets = weeksheet.collection_sheet_lines.select{|cs| cs.borrower_group_id == group[0]}.sort_by{|cs| cs.borrower_name} rescue []
+            group_amount, group_outstanding, group_installments, group_principal, group_interest, group_fee, group_due = 0, 0, 0, 0, 0, 0, 0
+            #table.data.push({"Actual Principal Due" => group ? group[1] : "No group"})
+            loan_row_count=0
+            collection_sheets.each do |ws|
+              table.data.push({
+                  "Name"                         => ws.borrower_name,
+                  "Loan Id"                      => ws.loan_id,
+                  "Status"                       => ws.loan_status.to_s.humanize,
+                  "Disbursed Amount"             => ws.loan_disbursed_amount.to_s,
+                  "Disbursed Date"               => ws.loan_disbursed_date,
+                  "Installment Number"           => ws.loan_installment_number,
+                  "Schedule Date"                => ws.loan_schedule_date,
+                  "Due Status"                   => ws.loan_due_status.to_s.humanize,
+                  "Days Past Due"                => ws.loan_days_past_due,
+                  "Schedule Principal Due"       => ws.loan_schedule_principal_due.to_s,
+                  "Actual Principal Due"         => ws.loan_actual_principal_due.to_s,
+                  "Actual Principal Outstanding" => ws.loan_actual_principal_outstanding.to_s,
+                  "Schedule Interest Due"        => ws.loan_schedule_interest_due.to_s,
+                  "Actual Interest Due"          => ws.loan_actual_interest_due.to_s,
+                  "Actual Interest Outstanding"  => ws.loan_actual_interest_outstanding.to_s,
+                  "Advance Amount"               => ws.loan_advance_amount.to_s,
+                  "Principal Receipts"           => ws.loan_principal_receipts.to_s,
+                  "Interest Receipts"            => ws.loan_interest_receipts.to_s,
+                  "Advance Receipts"             => ws.loan_advance_receipts.to_s,
+                  "Total Amount"                 => ws.total_amount_to_be_paid.to_s,
+                  "Signature" => "" })
+              loan_row_count     += 1
+              if loan_row_count==0
+                table.data.push({"Name" => ws.borrower_name, "Signature" => "", "Status" => "nothing outstanding"})
+              end
+            end
 
-          loans_to_disburse.each do |loan|
-            table.data.push({"amount" => loan.amount.to_currency, "name" => loan.client.name,
-                "group" => (loan.client.client_group or Nothing).name,
-                "loan product" => loan.loan_product.name, "first payment" => loan.scheduled_first_payment_date                              ,"spouse name" => loan.client.spouse_name
-              })
+            #          table.data.push({"amount" => group_amount.to_currency, "outstanding" => group_outstanding.to_currency,
+            #              "principal" => group_principal.to_currency, "interest" => group_interest.to_currency,
+            #              "fee" => group_fee.to_currency, "total due" => group_due.to_currency
+            #            })
+            #          tot_amount         += group_amount
+            #          tot_outstanding    += group_outstanding
+            #          tot_installments   += group_installments
+            #          tot_principal      += group_principal
+            #          tot_interest       += group_interest
+            #          tot_fee            += group_fee
+            #          total_due          += (group_principal + group_interest + group_fee)
+
           end
-          table.column_order  = ["name", "spouse name", "group", "amount", "loan product", "first payment", "signature"]
-          table.show_lines    = :all
-          table.shade_rows    = :none
-          table.show_headings = true
-          table.shade_headings = true
-          table.orientation   = :center
-          table.position      = :center
-          table.title_font_size = 16
-          table.header_gap = 10
-          pdf.text("\n")
-          pdf.text "Disbursements today"
-          pdf.text("\n")
+          #        table.data.push({"amount" => tot_amount.to_currency, "outstanding" => tot_outstanding.to_currency,
+          #            "principal" => tot_principal.to_currency,
+          #            "interest" => tot_interest.to_currency, "fee" => tot_fee.to_currency,
+          #            "total due" => (tot_principal + tot_interest + tot_fee).to_currency
+          #          })
+          #table.column_order  = ["name", "loan id" , "amount", "outstanding", "status", "disbursed", "installment", "principal", "interest", "fee", "total due", "days absent/total", "signature"]
+          table.column_order  = ["Name", "Loan Id" ,"Status","Disbursed Amount","Disbursed Date","Installment Number",
+            "Schedule Date","Due Status","Days Past Due","Schedule Principal Due",
+            "Actual Principal Due","Actual Principal Outstanding","Schedule Interest Due",
+            "Actual Interest Due","Actual Interest Outstanding","Advance Amount",
+            "Principal Receipts","Interest Receipts","Advance Receipts",
+            "Total Amount", "Signature"]
+          table.show_lines        = :all
+          table.show_headings     = true
+          table.shade_rows        = :none
+          table.shade_headings    = true
+          table.orientation       = :center
+          table.position          = :center
+          table.heading_font_size = 8
+          table.font_size         = 8
+          table.header_gap        = 10
+          table.maximum_width     = 830
           table.render_on(pdf)
+
+          idx += 1
         end
-        idx += 1
       end
       pdf.save_as(filename)
       return pdf
