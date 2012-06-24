2346f5b38d3b8fdc0da3f43f74b4c3adf59636c2
Krishnan Mani <km@krishnanm.com>
2012-06-24 12:09:03 +0530

    Spec updated for FacadeFactory

--- spec/models/improved/facade_factory_spec.rb
+++ spec/models/improved/facade_factory_spec.rb
@@ -4,20 +4,34 @@ describe FacadeFactory do
 
   before(:all) do
     @for_user = Factory(:user)
+    @facade_factory = FacadeFactory.instance
   end
 
   it "should return an instance of the requested StandardFacade" do
-    FacadeFactory::FACADE_TYPES.each { |key, value|
-      facade_instance = FacadeFactory.instance.get_instance(key, @for_user)
+    FacadeFactory::STANDARD_FACADES.each { |key, value|
+      facade_instance = @facade_factory.get_instance(key, @for_user)
       facade_instance.should be_an_instance_of value
     }
   end
 
+  it "should return an instance of the requested SingletonFacade" do
+    FacadeFactory::SINGLETON_FACADES.each { |key, value|
+      facade_instance = @facade_factory.get_instance(key, @for_user)
+      facade_instance.should be_an_instance_of value
+    }
+  end
+
+  it "should raise an error if it is requested for an instance of a facade that does not exist" do
+    non_existent_facade = :non_existent_facade
+    FacadeFactory::STANDARD_FACADES.include?(non_existent_facade).should be_false
+    FacadeFactory::SINGLETON_FACADES.include?(non_existent_facade).should be_false
+    lambda {@facade_factory.get_instance(:non_existent_facade)}.should raise_error
+  end
+
   it "should return an instance of the requested StandardFacade given an instance of another StandardFacade" do
-    facade_factory = FacadeFactory.instance
-    location_facade = facade_factory.get_instance(FacadeFactory::LOCATION_FACADE, @for_user)
-    facade_instance_requested = facade_factory.get_other_facade(FacadeFactory::MEETING_FACADE, location_facade)
-    facade_instance_requested.should be_an_instance_of FacadeFactory::FACADE_TYPES[FacadeFactory::MEETING_FACADE]
+    location_facade = @facade_factory.get_instance(FacadeFactory::LOCATION_FACADE, @for_user)
+    facade_instance_requested = @facade_factory.get_other_facade(FacadeFactory::MEETING_FACADE, location_facade)
+    facade_instance_requested.should be_an_instance_of FacadeFactory::STANDARD_FACADES[FacadeFactory::MEETING_FACADE]
   end
 
 end
