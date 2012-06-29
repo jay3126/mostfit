cee8a66a6d03f0eb6f1f3ecaf1e4a08f7828763b
Kirti Parihar <pariharkirti24@gmail.com>
2012-06-29 14:03:33 +0530

    Refactored generate_loans action, validations and error handling (needs to multiple times redirection in single method)

--- app/controllers/loan_files.rb
+++ app/controllers/loan_files.rb
@@ -11,63 +11,87 @@ class LoanFiles < Application
     display @loan_file
   end
 
-  #for generating 
+  # For generating loan
   def generate_loans(id)
-    return NotFound unless params['id'] 
+    # INITIALIZATION
+    @errors = []
+    return NotFound unless params['id']
     @loan_file = LoanFile.get(id)
     raise NotFound unless @loan_file
     @center = BizLocation.get(@loan_file.at_center_id)
     @branch = BizLocation.get(@loan_file.at_branch_id)
-    #    @clients = @loan_file.loan_applications.collect{|l| Client.get(l.client_id) if (l.loan.nil? && !(l.client_id.nil?))}.compact
     @clients = []
+
+    # GATE-KEEPING
+    if params[:loan]
+      applied_on_date = params[:loan][:applied_on]
+      scheduled_disbursal_date = params[:loan][:scheduled_disbursal_date]
+      scheduled_first_payment_date = params[:loan][:scheduled_first_payment_date]
+      applied_by_staff = params[:loan][:applied_by_staff_id]
+      recorded_by_user = session.user.id
+      clients = params[:clients]
+
+      # VALIDATIONS
+      @errors << "Applied on date must not be blank" if applied_on_date.blank?
+      @errors << "Scheduled disbursal date must not be blank" if scheduled_disbursal_date.blank?
+      @errors << "Scheduled first payment date must not be blank" if scheduled_first_payment_date.blank?
+      @errors << "Applied by staff must not be blank" if applied_by_staff.blank?
+      @errors << "Scheduled disbursal date must not before application date" if Date.parse(scheduled_disbursal_date) < Date.parse(applied_on_date)
+      @errors << "Scheduled first payment date must not before application date" if Date.parse(scheduled_first_payment_date) < Date.parse(applied_on_date)
+      @errors << "Scheduled first payment date must not before Scheduled disbursal date" if Date.parse(scheduled_first_payment_date) < Date.parse(scheduled_disbursal_date)
+    end
+
+    # need to refactor: get those clients only who are eligible
     @loan_file.loan_applications.each do |l|
       client = Client.get(l.client_id)
       not_eligible_client = client_facade.get_all_loans_for_counterparty(client)
       @clients << client if not_eligible_client.blank?
     end
-    sc = params[:clients].map{|k,v| k if v[:chosen]}.compact if params[:clients]   # nice to be able to say "if @selected_clients"
-    @selected_clients = sc.blank? ? nil : sc                                       # instead of "unless @selected_clients.blank?"
+
+    sc = clients.map{|k,v| k if v[:chosen]}.compact if clients
+    @selected_clients = sc.blank? ? nil : sc
+    debugger
     if params[:loan]
-      unless @selected_clients.blank?
-        begin
-          @loans = []
-          message = nil
-          @selected_clients.each do |client_id|
-            params[:clients][client_id].delete(:chosen)
-            lap = @loan_file.loan_applications(:client_id => client_id).first
-            applied_money_amount = MoneyManager.get_money_instance_least_terms(lap.amount.to_i)
-            from_lending_product = LendingProduct.get(params[:clients][client_id][:loan_product_id])
-            repayment_frequency = from_lending_product.repayment_frequency
-            tenure = from_lending_product.tenure
-            for_borrower = Client.get(client_id)
-            administered_at_origin = lap.at_center_id
-            accounted_at_origin = lap.at_branch_id
-            applied_on_date = params[:loan][:applied_on]
-            scheduled_disbursal_date = params[:loan][:scheduled_disbursal_date]
-            scheduled_first_payment_date = params[:loan][:scheduled_first_payment_date]
-            applied_by_staff = params[:loan][:applied_by_staff_id]
-            recorded_by_user = session.user.id
-            loan = loan_facade.create_new_loan(applied_money_amount,repayment_frequency,tenure,from_lending_product,for_borrower,administered_at_origin,accounted_at_origin,applied_on_date,scheduled_disbursal_date,scheduled_first_payment_date,applied_by_staff,recorded_by_user, nil)
-            @loans.push(loan)
+      if @errors.blank?
+        unless @selected_clients.blank?
+          begin
+            @loans = []
+            message = nil
+            @selected_clients.each do |client_id|
+              params[:clients][client_id].delete(:chosen)
+              lap = @loan_file.loan_applications(:client_id => client_id).first
+              applied_money_amount = MoneyManager.get_money_instance_least_terms(lap.amount.to_i)
+              from_lending_product = LendingProduct.get(params[:clients][client_id][:loan_product_id])
+              repayment_frequency = from_lending_product.repayment_frequency
+              tenure = from_lending_product.tenure
+              for_borrower = Client.get(client_id)
+              administered_at_origin = lap.at_center_id
+              accounted_at_origin = lap.at_branch_id
+              loan = loan_facade.create_new_loan(applied_money_amount,repayment_frequency,tenure,from_lending_product,for_borrower,administered_at_origin,accounted_at_origin,applied_on_date,scheduled_disbursal_date,scheduled_first_payment_date,applied_by_staff,recorded_by_user, nil)
+              @loans.push(loan)
+            end
+            r = @loans.map{|l| l.saved?}
+            if r.include?(false)
+              message = {:error => "Loans cannot be saved"}
+            else
+              @loan_file.update(:health_check_status => Constants::Status::READY_FOR_DISBURSEMENT)
+              loan_ids = @loans.map{|x| x.id}
+              message = {:notice => "Successfully added loans with ids #{loan_ids.to_json}"}
+            end
+          rescue => ex
+            message = {:error => "An error has occured: #{ex.message}"}
           end
-          r = @loans.map{|l| l.saved?}
-          if r.include?(false)
-            message = {:error => "Loans cannot be saved"}
-          else
-            @loan_file.update(:health_check_status => Constants::Status::READY_FOR_DISBURSEMENT)
-            loan_ids = @loans.map{|x| x.id}
-            message = {:notice => "Successfully added loans with ids #{loan_ids.to_json}"}
-          end
-        rescue => ex
-          message = {:error => "An error has occured: #{ex.message}"}
+          redirect resource(@loan_file), :message => message
+        else
+          display([@center, @clients], "loan_files/generate_loans")
         end
-        redirect resource(@loan_file), :message => message
       else
-        display([@center, @clients], "loan_files/generate_loans")
+        message = {:error => @errors.flatten.join(', ')}
+        redirect url("loan_files/generate_loans/#{params['id']}"), :message => message
       end
     else
       if @clients.include?(nil)
-        redirect resource(@loan_file), :message => {:error => 'Cannot generate loans for this loan file because clients have not been created for certain loan applications'} 
+        redirect resource(@loan_file), :message => {:error => 'Cannot generate loans for this loan file because clients have not been created for certain loan applications'}
       else
         display([@center, @clients], "loan_files/generate_loans")
       end
@@ -76,7 +100,7 @@ class LoanFiles < Application
 
   def loan_file_generation
     render :loan_file_generation
-  end 
+  end
 
   def pending_loan_file_generation
     @errors = {}
