7df41b62e1fa78be19ed9c7232eb710bc0cfd9aa
Krishnan Mani <km@krishnanm.com>
2012-07-03 13:54:30 +0530

    Now the check for valid assignment dates returns an explicit message indicating which items failed validation and the creation dates on these items, as well as the effective date of assignment

--- app/models/validators_assignments.rb
+++ app/models/validators_assignments.rb
@@ -1,11 +1,27 @@
 module Validators
   module Assignments
    
-    # Validates that the date of effective date of assignment does not precede the date of creation of any of the items participating in assignment
+    # Validates that the date of effective date of assignment is after the date of creation of any of the items to be assigned
     def self.is_valid_assignment_date?(effective_on, *to_be_assigned)
-      any_item_created_later = *to_be_assigned.any? {|item| effective_on < item.created_on}
-      any_item_created_later ? [false, "Some of the items to be assigned were created after #{effective_on}"] : 
-        true
+      if (to_be_assigned.length == 1)
+        item = to_be_assigned.first
+        test_val = effective_on < item.created_on ? [false, "#{item.to_s} created #{item.created_on}" + " has a creation date later than #{effective_on}"] :
+            true
+        return test_val
+      else
+        items_created_later = to_be_assigned.select {|item| effective_on < item.created_on}
+        return true if items_created_later.empty?
+        validation_message = ""
+        first_element = true
+        items_created_later.each {|item|
+          validation_message += " and " unless first_element
+          validation_message += "#{item.to_s} created #{item.created_on}"
+          first_element = false
+        }
+        validation_message += " has a creation date later than #{effective_on}"
+        return [false, validation_message]
+      end
+      true
     end
 
   end
--- spec/models/improved/validators_assignments_spec.rb
+++ spec/models/improved/validators_assignments_spec.rb
@@ -3,6 +3,10 @@ require File.join( File.dirname(__FILE__), '..', '..', "spec_helper" )
 class ToAssign
   attr_reader :created_on
   def initialize(created_on); @created_on = created_on; end
+
+  def to_s
+    "ToAssign instance"
+  end
 end
 
 describe Validators::Assignments do
@@ -14,7 +18,16 @@ describe Validators::Assignments do
     @to_assign_created_earlier = (1..10).each.collect {|num| ToAssign.new(@effective_on - num)}
   end
 
-  it "should allow assignment when the effective date of assignment does not precede the creation date of any of the items assigned" do
+  it "should return an error message as expected" do
+    effective_on = Date.parse('2012-04-01')
+    item = ToAssign.new(effective_on + 1)
+    message = "#{item.to_s} created #{item.created_on}" + " has a creation date later than #{effective_on}"
+    valid_val = Validators::Assignments.is_valid_assignment_date?(effective_on, item)
+    valid_val.first.should be_false
+    valid_val.last.should == message
+  end
+
+  it "should allow assignment when the effective date of assignment is after the creation date of any of the items assigned" do
     Validators::Assignments.is_valid_assignment_date?(@effective_on,
       *@to_assign_created_earlier).should be_true
   end
@@ -24,19 +37,16 @@ describe Validators::Assignments do
       *@to_assign_created_same_date).should be_true
   end
 
-  it "should disallow assignment when the effective date of assignment precedes the creation date of all items being assigned" do
+  it "should disallow assignment when the effective date of assignment is before the creation date of all items being assigned" do
     test_val = Validators::Assignments.is_valid_assignment_date?(@effective_on,
       *@to_assign_created_later)
-    test_val.should be_an_instance_of(Array)
     test_val.first.should be_false
   end
 
-  it "should disallow assignment when the effective date of assignment precedes the creation date of any item being assigned" do
+  it "should disallow assignment when the effective date of assignment is before the creation date of any item being assigned" do
     test_val = Validators::Assignments.is_valid_assignment_date?(@effective_on,
       @to_assign_created_later.first, @to_assign_created_earlier.first, @to_assign_created_earlier.last)
-    test_val.should be_an_instance_of(Array)
     test_val.first.should be_false
   end
-
 
 end
