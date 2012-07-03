b976c660ee1f0076b98e3cbdd1c4d7aa51e6b9c7
kaushal <kaushal@kaushal-RC420-RC520-RC720.(none)>
2012-07-03 17:16:49 +0530

    add creation_date field for Location Level on creation page

--- app/controllers/location_levels.rb
+++ app/controllers/location_levels.rb
@@ -18,19 +18,21 @@ class LocationLevels < Application
 
     # GATE-KEEPING
 
-    l_level =  params[:location_level][:level].to_i
-    l_name = params[:location_level][:name]
+    l_level         = params[:location_level][:level].to_i
+    l_name          = params[:location_level][:name]
+    l_creation_date = params[:location_level][:creation_date]
 
     # VALIDATIONS
 
     message[:error] = "Location Level cannot be blank !" if l_level.blank?
     message[:error] = "Location Level is not valid !" unless l_level == LocationLevel.level_number_for_new
     message[:error] = "Location Name cannot be blank !" if l_name.blank?
+    message[:error] = "Creation Date cannot be blank !" if l_creation_date.blank?
 
     # OPERATIONS PERFORMED
     if message[:error].blank?
       begin
-        location_level = LocationLevel.new(:level => l_level, :name => l_name)
+        location_level = LocationLevel.new(:level => l_level, :name => l_name, :creation_date => l_creation_date)
         if location_level.save
           message = {:notice => "Location Level successfully created"}
         else
