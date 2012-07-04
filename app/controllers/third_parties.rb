74e10aa77ff85c3ed1092717ea04f36326b808f4
Koustubh Sinkar <ksinkar@gmail.com>
2012-07-04 19:01:08 +0530

    Bug #2669: Resolved

--- app/controllers/third_parties.rb
+++ app/controllers/third_parties.rb
@@ -3,46 +3,29 @@ class ThirdParties < Application
   include DateParser
 
   def index
-    if session.user.role == :admin
-      @third_parties = @third_parties || ThirdParty.all
-      display @third_parties.sort! { |a,b| a.name.downcase <=> b.name.downcase }.paginate(:page => params[:page], :per_page => 15)
-    else
-      redirect(params[:return], :message => {:notice => "You dont have sufficient privilleges"})
-    end
+    @third_parties = @third_parties || ThirdParty.all
+    display @third_parties.sort! { |a,b| a.name.downcase <=> b.name.downcase }.paginate(:page => params[:page], :per_page => 15)
   end
 
   def show
-    if session.user.role == :admin
-      @third_party =ThirdParty.get(params[:id])
-      @user=User.get(@third_party.recorded_by).login
-      display @third_party
-    else
-      @third_parties = @third_parties || ThirdParty.all
-      redirect(params[:return], :message => {:notice => "You dont have sufficient privilleges"})
-    end
+    @third_party =ThirdParty.get(params[:id])
+    @user=User.get(@third_party.recorded_by).login
+    display @third_party
   end
-
+  
   def new
-    if session.user.role == :admin
-      @third_party=ThirdParty.new
-      display @third_party
-    else
-      redirect(params[:return], :message => {:notice => "You dont have sufficient privilleges"})
-    end
+    @third_party=ThirdParty.new
+    display @third_party
   end
-
+  
   def create
-    if session.user.role == :admin
-      @third_party = ThirdParty.new(params[:third_party])
-      @third_party.recorded_by = session.user.id
-      if @third_party.save
-        redirect(params[:return]||resource(:third_parties), :message => {:notice => "Third party '#{@third_party.name}' (Id:#{@third_party.id}) successfully created"})
-      else
-        message[:error] = "Third Party failed to be created"
-        render :new  # error messages will show
-      end
+    @third_party = ThirdParty.new(params[:third_party])
+    @third_party.recorded_by = session.user.id
+    if @third_party.save
+      redirect(params[:return]||resource(:third_parties), :message => {:notice => "Third party '#{@third_party.name}' (Id:#{@third_party.id}) successfully created"})
     else
-      redirect(params[:return], :message => {:notice => "You dont have sufficient privilleges"})
+      message[:error] = "Third Party failed to be created"
+      render :new  # error messages will show
     end
   end
 end
