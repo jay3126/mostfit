f0519a2c2a3d29811c05da53456898ba8d083ecf
Krishnan Mani <km@krishnanm.com>
2012-07-06 01:21:51 +0530

    ReportingFacade enchanced to return lists of loans by status
    In the event where no branches or centers are supplied, some of the queries have been corrected, since otherwise they are translated into SQL queries where the location is null

--- app/models/reporting_facade.rb
+++ app/models/reporting_facade.rb
@@ -11,16 +11,6 @@ class ReportingFacade < StandardFacade
     total_loan_allocation_receipts_at_locations_on_value_date(on_date, TRANSACTION_PERFORMED_AT, at_location_ids_ary)
   end
 
-  def total_loan_allocation_receipts_at_locations_on_value_date(on_date, performed_or_accounted_choice, *at_location_ids_ary)
-    total_loan_allocation_receipts_grouped_by_location = {}
-    property_sym = TRANSACTION_LOCATIONS[performed_or_accounted_choice]
-    at_location_ids_ary.each { |at_location_id|
-      all_loan_receipts_at_location = LoanReceipt.all(:effective_on => on_date, property_sym => at_location_id)
-      total_loan_allocation_receipts_grouped_by_location[at_location_id] = LoanReceipt.add_up(all_loan_receipts_at_location)
-    }
-    total_loan_allocation_receipts_grouped_by_location
-  end
-
   # Outstanding loan IDs by location
 
   def all_outstanding_loan_ids_accounted_at_locations_on_date(on_date, *at_location_ids_ary)
@@ -29,22 +19,6 @@ class ReportingFacade < StandardFacade
 
   def all_outstanding_loan_ids_administered_at_locations_on_date(on_date, *at_location_ids_ary)
     all_outstanding_loan_ids_at_locations_on_date(on_date, Constants::Loan::ADMINISTERED_AT, at_location_ids_ary)
-  end
-
-  def all_outstanding_loan_ids_at_locations_on_date(on_date, accounted_or_administered_choice, *at_location_ids_ary)
-    location_ids_array = *at_location_ids_ary.to_a
-    all_loan_ids_grouped_by_location = {}
-    location_ids_array.each { |at_location_id|
-      all_loans_at_location = []
-      case accounted_or_administered_choice
-        when Constants::Loan::ACCOUNTED_AT then all_loans_at_location = LoanAdministration.get_loans_accounted(at_location_id, on_date)
-        when Constants::Loan::ADMINISTERED_AT then all_loans_at_location = LoanAdministration.get_loans_administered(at_location_id, on_date)
-        else raise ArgumentError, "Please specify whether loans accounted or loans administered are needed"
-      end
-      all_outstanding_loans_at_location = all_loans_at_location.select {|loan| loan.is_outstanding_on_date?(on_date)}
-      all_loan_ids_grouped_by_location[at_location_id] = get_ids(all_outstanding_loans_at_location)
-    }
-    all_loan_ids_grouped_by_location
   end
 
   def all_outstanding_loans_on_date(on_date = Date.today)
@@ -77,16 +51,6 @@ class ReportingFacade < StandardFacade
 
   def all_outstanding_loans_balances_administered_at_locations_on_date(on_date, *at_location_ids_ary)
     all_outstanding_loans_balances_at_locations_on_date(on_date, Constants::Loan::ADMINISTERED_AT, at_location_ids_ary)
-  end
-
-  def all_outstanding_loans_balances_at_locations_on_date(on_date, accounted_or_administered_choice, *at_location_ids_ary)
-    location_ids_array = *at_location_ids_ary.to_a
-    all_loan_balances_grouped_by_location = {}
-    all_loans_grouped_by_location = all_outstanding_loan_ids_at_locations_on_date(on_date, accounted_or_administered_choice, *at_location_ids_ary)
-    all_loans_grouped_by_location.each { |at_location_id, for_loan_ids_ary|
-      all_loan_balances_grouped_by_location[at_location_id] = loan_balances_for_loan_ids_on_date(on_date, for_loan_ids_ary)
-    }
-    all_loan_balances_grouped_by_location
   end
 
   def loan_balances_for_loan_ids_on_date(on_date, *for_loan_ids_ary)
@@ -166,7 +130,7 @@ class ReportingFacade < StandardFacade
   # Loans by status at locations on date
 
   def loans_applied_by_branches_on_date(on_date, *at_branch_ids_ary)
-    loans_by_branches_for_status_on_date(:applied, on_date, *at_branch_ids_ary)
+    aggregate_loans_by_branches_for_status_on_date(:applied, on_date, *at_branch_ids_ary)
   end
 
   def loans_applied_by_centers_on_date(on_date, *at_center_ids_ary)
@@ -174,7 +138,7 @@ class ReportingFacade < StandardFacade
   end
 
   def loans_approved_by_branches_on_date(on_date, *at_branch_ids_ary)
-    loans_by_branches_for_status_on_date(:approved, on_date, *at_branch_ids_ary)
+    aggregate_loans_by_branches_for_status_on_date(:approved, on_date, *at_branch_ids_ary)
   end
 
   def loans_approved_by_centers_on_date(on_date, *at_center_ids_ary)
@@ -182,42 +146,108 @@ class ReportingFacade < StandardFacade
   end
 
   def loans_scheduled_for_disbursement_by_branches_on_date(on_date, *at_branch_ids_ary)
-    loans_by_branches_for_status_on_date(:scheduled_for_disbursement, on_date, *at_branch_ids_ary)
+    aggregate_loans_by_branches_for_status_on_date(:scheduled_for_disbursement, on_date, *at_branch_ids_ary)
   end
 
   def loans_scheduled_for_disbursement_by_centers_on_date(on_date, *at_center_ids_ary)
     loans_by_centers_for_status_on_date(:scheduled_for_disbursement, on_date, *at_center_ids_ary)
   end
-  
+
+  def individual_loans_disbursed_by_branches_on_date(on_date, *at_branch_ids_ary)
+    individual_loans_by_branches_for_status_on_date(:disbursed, on_date, *at_branch_ids_ary)
+  end
+
   def loans_disbursed_by_branches_on_date(on_date, *at_branch_ids_ary)
-    loans_by_branches_for_status_on_date(:disbursed, on_date, *at_branch_ids_ary)
+    aggregate_loans_by_branches_for_status_on_date(:disbursed, on_date, *at_branch_ids_ary)
+  end
+
+  def individual_loans_disbursed_by_centers_on_date(on_date, *at_center_ids_ary)
+    individual_loans_by_centers_for_status_on_date(:disbursed, on_date, *at_center_ids_ary)
   end
 
   def loans_disbursed_by_centers_on_date(on_date, *at_center_ids_ary)
     loans_by_centers_for_status_on_date(:disbursed, on_date, *at_center_ids_ary)
   end
+  
+  private
 
-  def loans_by_branches_for_status_on_date(for_status, on_date, *at_branch_ids_ary)
+  def individual_loans_by_branches_for_status_on_date(for_status, on_date, *at_branch_ids_ary)
+    loan_status, date_to_query, ignore_val = LoanLifeCycle::STATUSES_DATES_SUM_AMOUNTS[for_status]
+    query = {:status => loan_status, date_to_query => on_date}
+    query[:accounted_at_origin] = at_branch_ids_ary if (at_branch_ids_ary and (not(at_branch_ids_ary.empty?)))
+    all_loans = Lending.all(query)
+    # TODO: must be changed for loan administration
+    all_loans.group_by {|loan| loan.accounted_at_origin}
+  end
+
+  def individual_loans_by_centers_for_status_on_date(for_status, on_date, *at_center_ids_ary)
+    loan_status, date_to_query, ignore_val = LoanLifeCycle::STATUSES_DATES_SUM_AMOUNTS[for_status]
+    query = {:status => loan_status, date_to_query => on_date}
+    query[:administered_at_origin] = at_center_ids_ary if (at_center_ids_ary and (not(at_center_ids_ary.empty?)))
+    all_loans = Lending.all(query)
+    # TODO: must be changed for loan administration
+    all_loans.group_by {|loan| loan.administered_at_origin}
+  end
+
+  def aggregate_loans_by_branches_for_status_on_date(for_status, on_date, *at_branch_ids_ary)
     loan_status, date_to_query, amount_to_sum = LoanLifeCycle::STATUSES_DATES_SUM_AMOUNTS[for_status]
-    branches_array = *at_branch_ids_ary.to_a
-    query = Lending.all(:status => loan_status, date_to_query => on_date, :accounted_at_origin => branches_array)
-    count = query.count
-    sum_amount = query.aggregate(amount_to_sum)
+    query = {:status => loan_status, date_to_query => on_date}
+    query[:accounted_at_origin] = at_branch_ids_ary if (at_branch_ids_ary and (not (at_branch_ids_ary.empty?)))
+    query_results = Lending.all(query)
+
+    count = query_results.count
+    sum_amount = query_results.aggregate(amount_to_sum)
     sum_money_amount = sum_amount ? to_money_amount(sum_amount) : zero_money_amount
     {:count => count, :total_amount => sum_money_amount}
   end
 
   def loans_by_centers_for_status_on_date(for_status, on_date, *at_center_ids_ary)
     loan_status, date_to_query, amount_to_sum = LoanLifeCycle::STATUSES_DATES_SUM_AMOUNTS[for_status]
-    centers_array = *at_center_ids_ary.to_a
-    query = Lending.all(:status => loan_status, date_to_query => on_date, :administered_at_origin => centers_array)
-    count = query.count
-    sum_amount = query.aggregate(amount_to_sum)
+    query = {:status => loan_status, date_to_query => on_date}
+    query[:administered_at_origin] = at_center_ids_ary if (at_center_ids_ary and (not (at_center_ids_ary.empty?)))
+    query_results = Lending.all(query)
+
+    count = query_results.count
+    sum_amount = query_results.aggregate(amount_to_sum)
     sum_money_amount = sum_amount ? to_money_amount(sum_amount) : zero_money_amount
     {:count => count, :total_amount => sum_money_amount}
   end
 
-  private
+  def total_loan_allocation_receipts_at_locations_on_value_date(on_date, performed_or_accounted_choice, *at_location_ids_ary)
+    total_loan_allocation_receipts_grouped_by_location = {}
+    property_sym = TRANSACTION_LOCATIONS[performed_or_accounted_choice]
+    at_location_ids_ary.each { |at_location_id|
+      all_loan_receipts_at_location = LoanReceipt.all(:effective_on => on_date, property_sym => at_location_id)
+      total_loan_allocation_receipts_grouped_by_location[at_location_id] = LoanReceipt.add_up(all_loan_receipts_at_location)
+    }
+    total_loan_allocation_receipts_grouped_by_location
+  end
+
+  def all_outstanding_loans_balances_at_locations_on_date(on_date, accounted_or_administered_choice, *at_location_ids_ary)
+    location_ids_array = *at_location_ids_ary.to_a
+    all_loan_balances_grouped_by_location = {}
+    all_loans_grouped_by_location = all_outstanding_loan_ids_at_locations_on_date(on_date, accounted_or_administered_choice, *at_location_ids_ary)
+    all_loans_grouped_by_location.each { |at_location_id, for_loan_ids_ary|
+      all_loan_balances_grouped_by_location[at_location_id] = loan_balances_for_loan_ids_on_date(on_date, for_loan_ids_ary)
+    }
+    all_loan_balances_grouped_by_location
+  end
+
+  def all_outstanding_loan_ids_at_locations_on_date(on_date, accounted_or_administered_choice, *at_location_ids_ary)
+    location_ids_array = *at_location_ids_ary.to_a
+    all_loan_ids_grouped_by_location = {}
+    location_ids_array.each { |at_location_id|
+      all_loans_at_location = []
+      case accounted_or_administered_choice
+        when Constants::Loan::ACCOUNTED_AT then all_loans_at_location = LoanAdministration.get_loans_accounted(at_location_id, on_date)
+        when Constants::Loan::ADMINISTERED_AT then all_loans_at_location = LoanAdministration.get_loans_administered(at_location_id, on_date)
+        else raise ArgumentError, "Please specify whether loans accounted or loans administered are needed"
+      end
+      all_outstanding_loans_at_location = all_loans_at_location.select {|loan| loan.is_outstanding_on_date?(on_date)}
+      all_loan_ids_grouped_by_location[at_location_id] = get_ids(all_outstanding_loans_at_location)
+    }
+    all_loan_ids_grouped_by_location
+  end
 
   def to_money_amount(amount)
     Money.new(amount.to_i, default_currency)
