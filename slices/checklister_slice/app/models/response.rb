142dbce7560a746232b271f150594194e9b87d81
Rushabh Hathi <rushabhhathi@gmail.com>
2012-07-12 14:31:55 +0530

    method made to get deviation type

--- slices/checklister_slice/app/models/response.rb
+++ slices/checklister_slice/app/models/response.rb
@@ -1,18 +1,18 @@
 class Response
   include DataMapper::Resource
   COMPLETE_COMPLETION_STATUS = "complete"; INCOMPLETE_COMPLETION_STATUS = "incomplete"
-  PENDING_RESULT_STATUS="pending" ;CLEARED_RESULT_STATUS="cleared"
+  PENDING_RESULT_STATUS="pending"; CLEARED_RESULT_STATUS="cleared"
 
   COMPLETION_STATUSES=[COMPLETE_COMPLETION_STATUS, INCOMPLETE_COMPLETION_STATUS]
 
-  RESULT_STATUSES=[PENDING_RESULT_STATUS,CLEARED_RESULT_STATUS]
+  RESULT_STATUSES=[PENDING_RESULT_STATUS, CLEARED_RESULT_STATUS]
 
   #TODO: the place should be an id corresponding to master in the database
 
   property :id, Serial
-  property :created_at, DateTime ,:nullable=>false,:default=>Date.today
+  property :created_at, DateTime, :nullable => false, :default => Date.today
   property :deleted_at, DateTime
-  property :value_date, DateTime ,:nullable=>false,:default=>Date.today
+  property :value_date, DateTime, :nullable => false, :default => Date.today
   #
   property :completion_status, Enum.send('[]', *COMPLETION_STATUSES), :nullable => false, :index => true
   property :result_status, Enum.send('[]', *RESULT_STATUSES), :nullable => false, :index => true
@@ -27,6 +27,9 @@ class Response
   has n, :checklist_locations
 
 
+  def get_deviation_type
+    self.checklist.sections.first.dropdownpoints.first.dropdownpoint_fillings.all(:response_id => self.id).first.model_record_name
+  end
 
 
 end
