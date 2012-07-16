bd3fb186fdb33d789add732fd13d2fa2f7aefeb4
Rushabh Hathi <rushabhhathi@gmail.com>
2012-07-16 11:19:08 +0530

    Deviation Reports modified

--- app/models/reports/deviation_report.rb
+++ app/models/reports/deviation_report.rb
@@ -1,9 +1,9 @@
 class DeviationReport < Report
-  attr_accessor :from_date ,:to_date
+  attr_accessor :from_date, :to_date
+
   def initialize(params, dates, user)
     @from_date = (dates and dates[:from_date]) ? dates[:from_date] : Date.today - 30
     @to_date = (dates and dates[:to_date]) ? dates[:to_date] : Date.today - 30
-
 
 
   end
@@ -17,11 +17,17 @@ class DeviationReport < Report
   end
 
   def generate
-    @responses=Response.all(:value_date.lte=>@to_date, :value_date.gte=>@from_date)
-    debugger
-    data =@responses
+    if @to_date<@from_date
+      data="Validation"
+      return data
+    else
+      @responses=Response.all(:value_date.lte => @to_date, :value_date.gte => @from_date)
 
-    data
+
+      data =@responses
+
+      data
+    end
   end
 
 
