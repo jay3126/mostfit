ba59bdfd52b4b94ac59295dae30d1edd0c5ebabf
Kirti Parihar <pariharkirti24@gmail.com>
2012-07-24 14:00:39 +0530

    Trail balance report updated

--- app/models/reports/trial_balance.rb
+++ app/models/reports/trial_balance.rb
@@ -16,13 +16,14 @@ class TrialBalance < Report
   end
   
   def generate(param)
-    data = {}
-    default_currency = MoneyManager.get_default_currency
-    zero_credit_balance = LedgerBalance.zero_credit_balance(default_currency)
-    zero_debit_balance = LedgerBalance.zero_debit_balance(default_currency)
-
-    ledgers = Ledger.all
-    ledgers.group_by{|x| [x.account_type]}.each{|c, l| l.each{|x| data.merge!(c => {x => x.balance(Date.today)})}}
+    data = []
+    all_ledgers = Ledger.all
+    all_ledgers.each do |ledger|
+      ledger_balance = ledger.balance(@date)
+      if ledger_balance
+        data << ledger
+      end
+    end
     data
   end
 
