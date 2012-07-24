5f16fd53aa2d65b7fdec3cea57d9e95e652dcdad
Kirti Parihar <pariharkirti24@gmail.com>
2012-07-24 10:52:45 +0530

    Trial balance report

--- app/models/reports/trial_balance.rb
+++ app/models/reports/trial_balance.rb
@@ -1,21 +1,14 @@
 class TrialBalance < Report
   
-  attr_accessor :date, :branch_id
+  attr_accessor :date
 
   def initialize(params,dates, user)
     @date = (dates and dates[:date]) ? dates[:date] : Date.today
-    @branch_id = (params and params.key?(:branch_id) and not (params[:branch_id] == "")) ? params[:branch_id] : 0
     get_parameters(params, user)
   end
 
   def name
-    "Trial Balance for #{get_branch_name(@branch_id)} on #{@date}"
-  end
-
-  def get_branch_name(branch_id)
-    return "Head Office" if branch_id == 0
-    branch = Branch.get(branch_id)
-    branch ? branch.name : ""
+    "Trial Balance for date #{@date}"
   end
 
   def self.name
@@ -24,23 +17,13 @@ class TrialBalance < Report
   
   def generate(param)
     data = {}
-    accounts = Account.all(:branch_id => @branch_id)
-    accounts.each { |acc|
-      balance = acc.closing_balance_as_of(@date)
-      unless balance.nil?
-        if balance == 0
-          default_balance = acc.get_default_balance_type
-          debit_balance, credit_balance = ( default_balance == CREDIT_BALANCE ? [nil, 0] : [0, nil] )
-        else
-          debit_balance, credit_balance = ( balance > 0 ? [nil, balance] : [balance, nil] )
-        end
-      else
-        debit_balance, credit_balance = nil, nil
-      end
-      data[acc] = [debit_balance, credit_balance]
-    }
+    default_currency = MoneyManager.get_default_currency
+    zero_credit_balance = LedgerBalance.zero_credit_balance(default_currency)
+    zero_debit_balance = LedgerBalance.zero_debit_balance(default_currency)
+
+    ledgers = Ledger.all
+    ledgers.group_by{|x| [x.account_type]}.each{|c, l| l.each{|x| data.merge!(c => {x => x.balance(Date.today)})}}
     data
   end
 
-end   
-
+end
