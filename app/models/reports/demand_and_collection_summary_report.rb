e14e87656737fe8b10d6e490b614f0e178fa6a5d
jay3126 <janmejay.rai@intellecap.com>
2012-08-01 16:50:27 +0530

    Left over columns in Demand & Collection Summary done.

--- app/models/reports/demand_and_collection_summary_report.rb
+++ app/models/reports/demand_and_collection_summary_report.rb
@@ -45,6 +45,7 @@ class DemandAndCollectionSummaryReport < Report
       fee_dues = reporting_facade.all_aggregate_fee_dues_by_branches(@date, @date, *branch_id)
       loan_balances = reporting_facade.sum_all_outstanding_loans_balances_accounted_at_locations_on_date(@date, *branch_id)
       loan_allocations = reporting_facade.total_loan_allocation_receipts_accounted_at_locations_on_value_date(@date, *branch_id)
+      loan_installments = reporting_facade.loan_balances_for_loan_ids_on_date(@date, *branch_id)
 
       branch_data_map = {}
       branch_data_map[:loan_fee_receipts] = loan_fee_receipts
@@ -52,30 +53,10 @@ class DemandAndCollectionSummaryReport < Report
       branch_data_map[:fee_dues] = fee_dues
       branch_data_map[:loan_balances] = loan_balances
       branch_data_map[:loan_allocations] = loan_allocations
+      branch_data_map[:loan_installments] = loan_installments
 
       data[branch_id] = branch_data_map
     }
     data
-
-=begin
-Columns required in this report are as follows:
-1. Branch Name
-2. EWI Scheduled
-3. EWI Avance
-4. EWI Due 
-5. EWI Collected.
-6. Overdue for the day
-7. Overdue amount 
-8. Overdue EWI Collected.
-9. Fees Collectable
-10. Fees Collected.
-11. Other Fees Collected 
-12. Advance Collected
-13. Foreclosure POS
-14. Foreclosure Overdue Interest
-15. Total Collections
-16. Short Collections
-17. Differences
-=end
   end
 end
