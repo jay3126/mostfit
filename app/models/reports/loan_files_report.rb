e26b3a942a6d7301ca49790d12e9c60b82613f2a
Kirti Parihar <pariharkirti24@gmail.com>
2012-08-20 17:30:50 +0530

    Loan file report corrected

--- app/models/reports/loan_files_report.rb
+++ app/models/reports/loan_files_report.rb
@@ -1,10 +1,9 @@
 class LoanFilesReport < Report
-  attr_accessor :branch, :center, :branch_for_loan_application_report, :center_for_loan_application_report, :loan_file_status, :effective_date
+  attr_accessor :branch_for_loan_application_report, :center_for_loan_application_report, :effective_date
 
   def initialize(params, date, user)
     @date = date.blank? ? Date.today : date
     @name   = "Report on #{@date}"
-    @loan_file_status = params[:loan_file_status].gsub(' ',"_").downcase rescue ""
     @branch_id = params[:branch_for_loan_application_report] rescue ""
     @center_id = params[:center_for_loan_application_report] rescue ""
     get_parameters(params, user)
@@ -22,11 +21,10 @@ class LoanFilesReport < Report
     condition_hash = {}
     loan_file = {}
     count = 1
-    condition_hash.merge!(:health_check_status => @loan_file_status) unless @loan_file_status.blank?
     condition_hash.merge!(:at_center_id => @center_id) unless @center_id.blank?
     condition_hash.merge!(:at_branch_id => @branch_id) unless @branch_id.blank?
     loan_files = LoanFile.all(condition_hash)
-    loan_files.group_by{|x| [x.at_branch_id,x.at_center_id,x.health_check_status]}.each do |c, value|
+    loan_files.group_by{|x| [x.at_branch_id,x.at_center_id]}.each do |c, value|
       loan_file.merge!(count => {c[0] => { c[1] => { c[2] => value.count}}})
       count = count + 1
     end
