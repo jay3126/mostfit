b16609a106a70edd49976871576be4fe294aa9b1
jay3126 <janmejay.rai@intellecap.com>
2012-08-21 19:50:25 +0530

    Removed resources of the models which have been removed from router.rb

--- config/router.rb
+++ config/router.rb
@@ -95,13 +95,7 @@ Merb::Router.prepare do
   resources :loan_purposes
   resources :lending_products
   resources :lendings, :id => %r(\d+), :member => {:record_lending_preclose => [:put]}, :collection => {:write_off_lendings => [:get]}
-  resources :regions do
-    resources :areas do
-      resources :branches
-    end
-  end
   resources :staff_attendances
-  resources :areas
   resources :targets, :id => %r(\d+)
   resources :holidays
   resources :fees
@@ -120,11 +114,6 @@ Merb::Router.prepare do
   resources :client_groups do
     resources :grts
     resources :cgts
-  end
-  resources :loans, :id => %r(\d+), :member => {:prepay => [:get, :put]}, :collection => {:search_loan_by_id => [:get]}
-  resources :centers, :id => %r(\d+) do
-    resources :center_meeting_days
-    resources :center_cycles
   end
   resources :payments
   resources :branches, :id => %r(\d+)  do    
@@ -154,7 +143,6 @@ Merb::Router.prepare do
 
   match('/dashboard/centers/:report_type/:branch_id').to(:controller => 'dashboard', :action => "centers", :branch_id => ":branch_id", :report_type => ":report_type").name(:dashboard_centers)
   match('/design').to(:controller => 'loan_products', :action => 'design').name(:design_loan_product)
-  match('/centers/:id/groups(/:group_id).:format').to(:controller => 'centers', :action => 'groups')
 
   # maintainer slice
   slice(:maintainer, :path_prefix => "maintain")
@@ -175,12 +163,8 @@ Merb::Router.prepare do
 
   namespace :data_entry, :name_prefix => 'enter' do  # for url(:enter_payment) and the likes
     match('/clients(/:action)(/:id)(.:format)').to(:controller => 'clients').name(:clients)
-    match('/loans/approve_by_center/:id').to(:controller => 'loans', :action => 'approve').name(:approval_by_center)
-    match('/loans(/:action)(.:format)').to(:controller => 'loans').name(:loans)
     match('/payments(/:action)(.:format)').to(:controller => 'payments').name(:payments)
     match('/attendancy(/:action)(.:format)').to(:controller => 'attendancy').name(:attendancy)
-    match('/branches(/:action)(/:id)(.:format)').to(:controller => 'branches').name(:branches)
-    match('/centers(/:action)(/:id)(.:format)').to(:controller => 'centers').name(:centers)
     match('/groups(/:action)(/:id)(.:format)').to(:controller => 'client_groups').name(:groups)
     match('/client_groups(/:action)(/:id)(.:format)').to(:controller => 'client_groups').name(:groups)
   end
@@ -195,17 +179,13 @@ Merb::Router.prepare do
   match('/preferred_locale').to(:controller => "users", :action => 'preferred_locale').name(:preferred_locale)
   match('/graph_data/:action(/:id)').to(:controller => 'graph_data').name(:graph_data)
   match('/staff_members/:id/centers').to(:controller => 'staff_members', :action => 'show_centers').name(:show_staff_member_centers)
-  match('/branches/:id/today').to(:controller => 'branches', :action => 'today').name(:branch_today)
   match('/entrance(/:action)').to(:controller => 'entrance').name(:entrance)
-  match('/branches/:branch_id/centers/:center_id/clients/:client_id/test').to( :controller => 'loans', :action => 'test')
-  match('/branches/:branch_id/centers/:center_id/weeksheet').to( :controller => 'centers', :action => 'weeksheet').name(:weeksheet)
   match('/staff_members/:id/day_sheet').to(:controller => 'staff_members', :action => 'day_sheet').name(:day_sheet)
   match('/staff_members/:id/day_sheet.:format').to(:controller => 'staff_members', :action => 'day_sheet', :format => ":format").name(:day_sheet_with_format)
   match('/staff_members/:id/disbursement_sheet').to(:controller => 'staff_members', :action => 'disbursement_sheet').name(:disbursement_sheet)
   match('/staff_members/:id/disbursement_sheet.:format').to(:controller => 'staff_members', :action => 'disbursement_sheet', :format => ":format").name(:disbursement_sheet_with_format)
   match('/browse(/:action)(.:format)').to(:controller => 'browse').name(:browse)
   match('/home(/:action)(.:format)').to(:controller => 'home').name(:home)
-  match('/loans/:action').to(:controller => 'loans').name(:loan_actions)
   match('/client/:action').to(:controller => 'clients').name(:client_actions)
   # this uses the redirect_to_show methods on the controllers to redirect some models to their appropriate urls
   match('/documents/:action(/:id)').to(:controller => "documents").name(:documents_action_link)
@@ -225,10 +205,6 @@ Merb::Router.prepare do
   match('/auth_override_reasons/:action').to(:controller => 'auth_override_reasons').name(:auth_override_reason_actions)
   match('/auth_override_reasons/new').to(:controller => 'auth_override_reasons', :action => 'new')
 
-  #cachers
-  match('/cachers/:action').to(:controller => 'cachers').name(:caches)
-  #match('/loan_files/:action').to(:controller => 'loan_files').name(:loan_files)
-
   #API Route
   match('/api/v1') do
     match('/browse.:format').to(:controller => 'browse', :action => 'index')
@@ -236,36 +212,16 @@ Merb::Router.prepare do
     match('/staff_members.:format').to(:controller => 'staff_members', :action =>'index')
     match('/staff_members/:id.:format').to(:controller => 'staff_members', :action =>'show')
     match('/data_entry/payments/by_center.:format').to(:controller => 'data_entry/payments', :action =>'by_center')
-    match('/regions.:format').to(:controller => 'regions', :action =>'index')
-    match('/regions/:id.:format').to(:controller => 'regions', :action =>'show')
-    match('/areas.:format').to(:controller => 'areas', :action =>'index')
-    match('/areas/:id.:format').to(:controller => 'areas', :action =>'show')
     match('/transaction_logs.:format').to(:controller => 'transaction_logs', :action =>'index')
     match('/transaction_logs/:id.:format').to(:controller => 'transaction_logs', :action =>'show')
     match('/model_event_logs.:format').to(:controller => 'model_event_logs', :action =>'index')
     match('/model_event_logs/:id.:format').to(:controller => 'model_event_logs', :action =>'show')
-    match('/branches') do
-      match('.:format').to(:controller => 'branches', :action =>'index')
-      match('/:id.:format').to(:controller => 'branches', :action =>'show')
-      match('/:branch_id/centers', :method => "get") do 
-        match('.:format').to(:controller => 'centers', :action =>'index')
-        match('/:id.:format').to(:controller => 'centers', :action =>'show')
-      end
-    end
     match('/client_groups.:format', :method => "get").to(:controller => 'client_groups', :action =>'index')
     match('/client_groups/:id.:format').to(:controller => 'client_groups', :action =>'show')
-    match('/branches/:branch_id/centers/:center_id/clients/:id.:format', :method => "get").to(:controller => 'clients', :action =>'show')
-    match('/branches/:branch_id/centers/:id.:format').to(:controller => 'centers', :action =>'show')
-    match('/loans/:id.:format').to(:controller => 'loans', :action =>'show')
     match('/users.:format').to(:controller => 'users', :action =>'index')
     match('/loan_products.:format').to(:controller => 'loan_products', :action =>'index')
     match('/loan_products/:id.:format').to(:controller => 'loan_products', :action =>'show')
-    match('/branches/:branch_id/centers/:center_id/clients/:client_id/loans/:loan_id/payments.:format').to(:controller => 'payments', :action =>'create')
-    match('/branches/:branch_id/centers/:center_id/clients/:id.:format', :method => "put").to(:controller => 'clients', :action =>'update')
-    match('/branches/:branch_id/centers/:center_id/clients.:format', :method => "post").to(:controller => 'clients', :action =>'create')
     match('/attendance.:format', :method => "post").to(:controller => 'attendances', :action =>'create')
-    match('/branches/:branch_id/centers/:center_id/clients/:client_id/loans.:format', :method => "post").to(:controller => 'loans', :action =>'create')
-    match('/centers.:format', :method => "post").to(:controller => 'centers', :action =>'create')
     match('/client_groups.:format', :method => "post").to(:controller => 'client_groups', :action =>'create')
     match('/holidays.:format', :method => "get").to(:controller => 'holidays', :action =>'index')
     match('/handshake.:format', :method => "get").to(:controller => 'entrance', :action =>'handshake')
