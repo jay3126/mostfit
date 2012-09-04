848a592075fd7682e680fa51b380c28f2feeb45a
kaushal <kaushal@kaushal-RC420-RC520-RC720.(none)>
2012-09-04 12:17:02 +0530

    update simple_fee_product UI and add precentage option in simple_fee_product

--- app/controllers/simple_fee_products.rb
+++ app/controllers/simple_fee_products.rb
@@ -14,20 +14,36 @@ class SimpleFeeProducts < Application
     name            = params[:simple_fee_product][:name]
     fee_charge_type = params[:simple_fee_product][:fee_charged_on_type]
     created_on      = params[:simple_fee_product][:created_on]
+    fee_amount      = params[:timed_amount][:fee_only_amount]
+    tax_amount      = params[:timed_amount][:tax_only_amount]
+    amount_type     = params[:timed_amount][:amount_type]
+    percentage      = params[:timed_amount][:percentage]
+    currency        = 'INR'
 
     #VALIDATION
     @message[:error] = 'Created On cannot be blank' if created_on.blank?
     @message[:error] = 'Fee Charge Type cannot be blank' if fee_charge_type.blank?
     @message[:error] = 'Name cannot be blank' if name.blank?
+    @message[:error] = 'Fee Amount cannot be blank' if fee_amount.blank? && amount_type == 'fix_amount'
+    @message[:error] = 'Tax Amount cannot be blank' if tax_amount.blank? && amount_type == 'fix_amount'
+    @message[:error] = 'Percentage Amount cannot be blank' if percentage.blank? && amount_type != 'fix_amount'
 
     #PERFORM OPERTION
     begin
       if @message[:error].blank?
         fee = SimpleFeeProduct.new(:name => name, :fee_charged_on_type => fee_charge_type, :created_on => created_on)
         if fee.save
+          if amount_type == 'fix_amount'
+            fee_money_amount = MoneyManager.get_money_instance(fee_amount)
+            tax_money_amount = MoneyManager.get_money_instance(tax_amount)
+            fee_time_amount = fee.timed_amounts.new(:amount_type => amount_type, :fee_only_amount => fee_money_amount.amount, :tax_only_amount => tax_money_amount.amount , :currency => currency, :effective_on => created_on)
+          else
+            fee_time_amount = fee.timed_amounts.new(:amount_type => amount_type, :percentage => percentage.to_f, :currency => currency, :effective_on => created_on)
+          end
+          fee_time_amount.save
           @message[:notice] = 'Fee Product saved successfully'
         else
-          @message[:error] = 'Fee Product saved fail'
+          @message[:error] = fee.errors.first.join(', ')
         end
       end
     rescue => ex
@@ -35,7 +51,42 @@ class SimpleFeeProducts < Application
     end
 
     #REDIREATION/RENDER
-    redirect resource(:simple_fee_products), :message => @message
+    if @message[:error].blank?
+      redirect resource(fee), :message => {:error => @message[:notice]}
+    else
+      redirect resource(:simple_fee_products), :message => {:notice => @message[:error]}
+    end
+
+  end
+
+  def update
+    #VARIABLES THAT IS USED THROUGHTOUT
+    @message = {}
+
+    #GET-KEEPING
+    fee_product_id  = params[:id]
+    fee_name        = params[:simple_fee_product][:name]
+    @fee_product    = SimpleFeeProduct.get fee_product_id
+
+    #VALIDATION
+    @message[:error] = 'Name cannot be blank' if fee_name.blank?
+
+    #PERFORM OPERTION
+    begin
+      if @message[:error].blank?
+        @fee_product.name = fee_name
+        if @fee_product.save
+          @message[:notice] = 'Fee Product updated successfully'
+        else
+          @message[:error] = @fee_product.errors.first.join(', ')
+        end
+      end
+    rescue => ex
+      @message = {:error => "An error has occured: #{ex.message}"}
+    end
+
+    #REDIREATION/RENDER
+    redirect resource(@fee_product), :message => @message
 
   end
 
@@ -51,29 +102,37 @@ class SimpleFeeProducts < Application
     @message = {}
 
     #GET-KEEPING
-    fee_product_id = params[:id]
-    fee_amount     = params[:timed_amount][:fee_only_amount]
-    tax_amount     = params[:timed_amount][:tax_only_amount]
-    currency       = params[:timed_amount][:currency]
-    effective_on   = params[:timed_amount][:effective_on]
-    fee_product    = SimpleFeeProduct.get fee_product_id
+    fee_product_id  = params[:id]
+    fee_amount      = params[:timed_amount][:fee_only_amount]
+    tax_amount      = params[:timed_amount][:tax_only_amount]
+    amount_type     = params[:timed_amount][:amount_type]
+    percentage      = params[:timed_amount][:percentage]
+    effective_on    = params[:timed_amount][:effective_on]
+    fee_product     = SimpleFeeProduct.get fee_product_id
+    currency        = 'INR'
 
     #VALIDATION
+    @message[:error] = 'Amount Type cannot be blank' if amount_type.blank?
     @message[:error] = 'Effective On cannot be blank' if effective_on.blank?
     @message[:error] = 'Currency cannot be blank' if currency.blank?
-    @message[:error] = 'Tax amount cannot be blank' if tax_amount.blank?
-    @message[:error] = 'Fee amount cannot be blank' if fee_amount.blank?
+    @message[:error] = 'Fee Amount cannot be blank' if fee_amount.blank? && amount_type == 'fix_amount'
+    @message[:error] = 'Tax Amount cannot be blank' if tax_amount.blank? && amount_type == 'fix_amount'
+    @message[:error] = 'Percentage Amount cannot be blank' if percentage.blank? && amount_type != 'fix_amount'
 
     #PERFORM OPERTION
     begin
       if @message[:error].blank?
-        fee_money_amount = MoneyManager.get_money_instance(fee_amount)
-        tax_money_amount = MoneyManager.get_money_instance(tax_amount)
-        fee_time_amount = fee_product.timed_amounts.new(:fee_only_amount => fee_money_amount.amount, :tax_only_amount => tax_money_amount.amount , :currency => currency, :effective_on => effective_on)
+        if amount_type == 'fix_amount'
+          fee_money_amount = MoneyManager.get_money_instance(fee_amount)
+          tax_money_amount = MoneyManager.get_money_instance(tax_amount)
+          fee_time_amount = fee_product.timed_amounts.new(:amount_type => amount_type, :fee_only_amount => fee_money_amount.amount, :tax_only_amount => tax_money_amount.amount , :currency => currency, :effective_on => effective_on)
+        else
+          fee_time_amount = fee_product.timed_amounts.new(:amount_type => amount_type, :percentage => percentage.to_f, :currency => currency, :effective_on => effective_on)
+        end
         if fee_time_amount.save
           @message[:notice] = 'Fee Time Amount saved successfully'
         else
-          @message[:error] = 'Fee Time Amount saved fail'
+          @message[:error] = fee_time_amount.errors.first.join(', ')
         end
       end
     rescue => ex
