dc6fd8701727c5d55c22c199ab795d3bcec4e663
Kirti parihar <pariharkirti24@gmail.com>
2012-09-14 15:42:23 +0530

    Added method in global helper to fetch all funder with respective funder lines

--- app/models/tranch.rb
+++ app/models/tranch.rb
@@ -17,31 +17,4 @@ class Tranch
 
   def created_on; self.date_of_commencement; end
 
-=begin
- 1 class Photo
- 2   include DataMapper::Resource
- 3
- 4   property :id, Serial
- 5
- 6   has n, :taggings
- 7   has n, :tags, :through => :taggings
- 8 end
- 9
-10 class Tag
-11   include DataMapper::Resource
-12
-13   property :id, Serial
-14
-15   has n, :taggings
-16   has n, :photos, :through => :taggings
-17 end
-18
-19 class Tagging
-20   include DataMapper::Resource
-21
-22   belongs_to :tag,   :key => true
-23   belongs_to :photo, :key => true
-24 end
-=end
-
 end
