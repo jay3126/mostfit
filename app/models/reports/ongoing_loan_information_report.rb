31ac6c9e72087bcb71209fdde706d53cacb7e241
jay3126 <janmejay.rai@intellecap.com>
2012-09-21 01:38:30 +0530

    Ongoing Loan Information Report revamped using Funding Line filter.

--- app/models/reports/ongoing_loan_information_report.rb
+++ app/models/reports/ongoing_loan_information_report.rb
@@ -1,15 +1,14 @@
 class OngoingLoanInformationReport < Report
 
-  attr_accessor :from_date, :to_date, :biz_location_branch
+  attr_accessor :from_date, :to_date, :funding_line_id
+
+  validates_with_method :funding_line_id, :funding_line_not_selected
 
   def initialize(params, dates, user)
     @from_date = (dates and dates[:from_date]) ? dates[:from_date] : Date.today - 7
     @to_date   = (dates and dates[:to_date]) ? dates[:to_date] : Date.today
     @name = "Ongoing Loan Information Report from #{@from_date} to #{@to_date}"
     @user = user
-    location_facade = get_location_facade(@user)
-    all_branch_ids = location_facade.all_nominal_branches.collect {|branch| branch.id}
-    @biz_location_branch = (params and params[:biz_location_branch] and (not (params[:biz_location_branch].empty?))) ? params[:biz_location_branch] : all_branch_ids
     get_parameters(params, user)
   end
 
@@ -39,10 +38,10 @@ class OngoingLoanInformationReport < Report
     location_facade  = get_location_facade(@user)
     data = {}
 
-    params = {:accounted_at_origin => @biz_location_branch, :status => :disbursed_loan_status}
-    lending_ids = Lending.all(params).aggregate(:id)
-    lending_ids.each do |l|
+    loan_ids = FundingLineAddition.all(:funding_line_id => @funding_line_id).aggregate(:lending_id)
+    loan_ids_with_disbursal_dates = Lending.all(:id => loan_ids, :disbursal_date.gte => @from_date, :disbursal_date.lte => @to_date).aggregate(:id)
 
+    loan_ids_with_disbursal_dates.each do |l|
       loan = Lending.get(l)
       client = loan.loan_borrower.counterparty
       client_id = client ? client.id : "Not Specified"
@@ -52,10 +51,10 @@ class OngoingLoanInformationReport < Report
       branch = BizLocation.get(loan.accounted_at_origin)
       branch_name = branch ? branch.name : "Not Specified"
       branch_id = branch ? branch.id : "Not Specified"
-      area = location_facade.get_parent(branch, @date)
-      district = location_facade.get_parent(area, @date)
-      district_name = district ? district.name : "Not Specified"
-      district_id = district ? district.id : "Not Specified"
+      location = LocationLink.all_parents(branch, @to_date)
+      district = location.select{|s| s.location_level.name.downcase == 'district'}.first
+      district_name = (district and (not district.blank?)) ? district.name : "Not Specified"
+      district_id = (district and (not district.blank?)) ? district.id : "Not Specified"
       no_of_installments_remaining = reporting_facade.number_of_installments_per_loan(loan.id)
       principal_outstanding_beginning_of_week = loan.scheduled_principal_outstanding(@from_date)
 
@@ -71,10 +70,10 @@ class OngoingLoanInformationReport < Report
         principal_paid_during_week = loan.principal_received_till_date(@to_date) - loan.principal_received_till_date(@from_date)
       end
 
-      if loan.scheduled_principal_outstanding(@from_date) > loan.actual_principal_outstanding
-        principal_overdue = loan.scheduled_principal_outstanding(@from_date) - loan.actual_principal_outstanding
+      if loan.scheduled_principal_outstanding(@to_date) > loan.actual_principal_outstanding(@to_date)
+        principal_overdue = loan.scheduled_principal_outstanding(@to_date) - loan.actual_principal_outstanding(@to_date)
       else
-        principal_overdue = loan.actual_principal_outstanding - loan.scheduled_principal_outstanding(@from_date)
+        principal_overdue = loan.actual_principal_outstanding(@to_date) - loan.scheduled_principal_outstanding(@to_date)
       end
 
       number_of_days_principal_overdue = loan.days_past_due_on_date(@from_date)
@@ -93,10 +92,10 @@ class OngoingLoanInformationReport < Report
         interest_paid_during_week = loan.interest_received_till_date(@to_date) - loan.interest_received_till_date(@from_date)
       end
 
-      if loan.scheduled_interest_outstanding(@from_date) > loan.actual_interest_outstanding
-        interest_overdue = loan.scheduled_interest_outstanding(@from_date) - loan.actual_interest_outstanding
+      if loan.scheduled_interest_outstanding(@to_date) > loan.actual_interest_outstanding(@to_date)
+        interest_overdue = loan.scheduled_interest_outstanding(@to_date) - loan.actual_interest_outstanding(@to_date)
       else
-        interest_overdue = loan.actual_interest_outstanding - loan.scheduled_interest_outstanding(@from_date)
+        interest_overdue = loan.actual_interest_outstanding(@to_date) - loan.scheduled_interest_outstanding(@to_date)
       end
 
       number_of_days_interest_overdue = loan.days_past_due_on_date(@from_date)
@@ -105,5 +104,10 @@ class OngoingLoanInformationReport < Report
       data[loan] = {:client_id => client_id, :client_name => client_name, :loan_id => loan_id, :district_id => district_id, :district_name => district_name, :branch_id => branch_id, :branch_name => branch_name, :no_of_installments_remaining => no_of_installments_remaining, :principal_outstanding_beginning_of_week => principal_outstanding_beginning_of_week, :principal_due_during_week => principal_due_during_week, :principal_paid_during_week => principal_paid_during_week, :principal_overdue => principal_overdue, :number_of_days_principal_overdue => number_of_days_principal_overdue, :current_principal_outstanding => current_principal_outstanding, :interest_outstanding_beginning_of_week => interest_outstanding_beginning_of_week, :interest_due_during_week => interest_due_during_week, :interest_paid_during_week => interest_paid_during_week, :interest_overdue => interest_overdue, :number_of_days_interest_overdue => number_of_days_interest_overdue, :current_interest_outstanding => current_interest_outstanding, :loan_lan => loan_lan}
     end
     data
+  end
+
+  def funding_line_not_selected
+    return [false, "Please select Funding Line"] if self.respond_to?(:funding_line_id) and not self.funding_line_id
+    return true
   end
 end
