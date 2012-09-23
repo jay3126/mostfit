442ef6fa9b2797cad830a8dec45171e1a99e57da
Kaushal Kishor Sharma <kaushal_kishor16@yahoo.com>
2012-09-23 17:49:12 +0530

    create meeting calendar on meeting schedule creation time

--- app/controllers/meeting_schedules.rb
+++ app/controllers/meeting_schedules.rb
@@ -22,10 +22,11 @@ class MeetingSchedules < Application
     meeting_begin_on            = Date.parse params[:meeting_schedule][:schedule_begins_on]
     meeting_time_begins_hours   = params[:meeting_schedule][:meeting_time_begins_hours].to_i
     meeting_time_begins_minutes = params[:meeting_schedule][:meeting_time_begins_minutes].to_i
+    meeting_numbers             = params[:meeting_schedule][:meeting_numbers].to_i
     @biz_location               = BizLocation.get params[:biz_location_id]
 
     # VALIDATIONS
-
+    message[:error] = "No. Of Meeting must be positive" if meeting_numbers < 0
     message[:error] = "Please fill right value of time" unless Constants::Time::MEETING_HOURS_PERMISSIBLE_RANGE.include?(meeting_time_begins_hours) &&
       Constants::Time::MEETING_MINUTES_PERMISSIBLE_RANGE.include?(meeting_time_begins_minutes)
     message[:error] = "Bigin Date cannot be holiday" unless configuration_facade.permitted_business_days_in_month(meeting_begin_on).include?(meeting_begin_on)
@@ -35,7 +36,7 @@ class MeetingSchedules < Application
     if message[:error].blank?
       begin
         msi = MeetingScheduleInfo.new(meeting_schedule, meeting_begin_on, meeting_time_begins_hours, meeting_time_begins_minutes)  
-        if mf.setup_meeting_schedule @biz_location, msi
+        if mf.setup_meeting_schedule(@biz_location, msi, meeting_numbers)
           message = {:notice => "Center Meeing Schedule successfully created"}
         else
           message = {:error => "Center Meeting Schedule creation fail"}
