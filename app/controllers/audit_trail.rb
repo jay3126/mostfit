65e5b07fccb29f802c8f60d002b4697b69980b08
jay3126 <janmejay.rai@intellecap.com>
2012-10-06 22:40:01 +0530

    Audit Trail enabled for Reason

--- app/controllers/audit_trail.rb
+++ app/controllers/audit_trail.rb
@@ -33,6 +33,7 @@ class AuditTrails < Application
     model = "AssetCategory" if (params[:audit_for][:controller] == "asset_categories")
     model = "AssetSubCategory" if (params[:audit_for][:controller] == "asset_sub_categories")
     model = "AssetType" if (params[:audit_for][:controller] == "asset_types")
+    model = "Reason" if (params[:audit_for][:controller] == "reasons")
     model = "Lending" if not ["BizLocation", "Lending", "Client", "PaymentTransaction"].include?(model) and /Lending^/.match(model)   
 
     if (params[:audit_for][:controller] == "center_cycles")
