7f75d45add78f029267ad6eddbeae38fa95c055f
jay3126 <janmejay.rai@intellecap.com>
2012-10-09 03:44:11 +0530

    SQL script to delete some unwanted pre-existing columns, checked in. Please use this script to delete some un-wanted column in location_holidays table. Sqript is in the following file:
    lib/tasks/alter_location_holidays_table.sql
    
    Ignore if using a fresh database.

---
+++ lib/tasks/alter_location_holidays_table.sql
@@ -0,0 +1,4 @@
+ALTER TABLE `location_holidays` DROP COLUMN `location_type`;
+ALTER TABLE `location_holidays` DROP COLUMN `location_id`;
+ALTER TABLE `location_holidays` DROP COLUMN `created_by_user_id`;
+ALTER TABLE `location_holidays` DROP COLUMN `biz_location_id`;
