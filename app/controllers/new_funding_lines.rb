5702d815ed7872e752c58a97507dcbacb49379e5
Kirti parihar <pariharkirti24@gmail.com>
2012-10-09 11:57:51 +0530

    Redirection for Edit and create of funding line and tranch

--- app/controllers/new_funding_lines.rb
+++ app/controllers/new_funding_lines.rb
@@ -21,24 +21,32 @@ class NewFundingLines < Application
   def create
     # INITIALIZING VARIABLES USED THROUGHOUT
     message = {}
+    @errors = []
     # GATE-KEEPING
     funder_id = params[:new_funder_id]
     amount_str = params[:amount]
     sanction_date = params[:new_funding_line][:sanction_date]
-    begin
-      @money = MoneyManager.get_money_instance(amount_str)
-      amount = @money.amount
-      currency = @money.currency
-      @funder = NewFunder.get(funder_id)
-      @funding_line = @funder.new_funding_lines.new({:amount => amount, :currency => currency, :new_funder_id => funder_id, :created_by => session.user.id, :sanction_date => sanction_date})
-      message = {:notice => "Funding Line was successfully created"}
-    rescue => ex
-      message = {:error => ex.message}
-    end
-    if @funding_line.save
-      redirect resource(:new_funders), :message => message
+    @funder = NewFunder.get(funder_id)
+
+    # VALIDATIONS
+    @errors << "Amount must not be blank" if amount_str.blank?
+    @errors << "Sanction date must not be blank" if sanction_date.blank?
+
+    # OPERATIONS-PERFORMED
+    if @errors.blank?
+      begin
+        @money = MoneyManager.get_money_instance(amount_str)
+        amount = @money.amount
+        currency = @money.currency
+        @funding_line = @funder.new_funding_lines.new({:amount => amount, :currency => currency, :new_funder_id => funder_id, :created_by => session.user.id, :sanction_date => sanction_date})
+        message = {:notice => "Funding Line was successfully created"}
+      rescue => ex
+        message = {:error => ex.message}
+      end
+      redirect url("new_funders/#{@funder.id}"), :message => message
     else
-      redirect url("new_funding_lines/new?#{funder_id}"), :message => {:error => "#{@funding_line.errors.instance_variable_get("@errors").map{|k, v| v.join(", ")}.join(", ")}"}
+      @funding_line = @funder.new_funding_lines.new({:amount => amount, :currency => currency, :new_funder_id => funder_id, :created_by => session.user.id, :sanction_date => sanction_date})
+      redirect url("new_funders/#{@funder.id}/new_funding_lines/new"), :message => {:error => @errors.flatten.join(', ')}
     end
   end
 
@@ -59,7 +67,7 @@ class NewFundingLines < Application
     currency = @money.currency
     sanction_date = params[:new_funding_line][:sanction_date]
     if @funding_line.update_attributes({:amount => amount, :currency => currency, :new_funder_id => @funder.id, :created_by => session.user.id, :sanction_date => sanction_date})
-      redirect url("new_funders/#{@funder.id}/new_funding_lines/#{@funding_line.id}"), :message => {:notice => "Funding Line details updated successfully"}
+      redirect url("new_funders/#{@funder.id}"), :message => {:notice => "Funding Line details updated successfully"}
     else
       redirect url("new_funders/#{@funder.id}/new_funding_lines/#{@funding_line.id}/edit"), :message => {:error => "Details of Funding Line cannot be updated because: #{@funding_line.errors.instance_variable_get("@errors").map{|k, v| v.join(", ")}.join(", ")}"}
     end
