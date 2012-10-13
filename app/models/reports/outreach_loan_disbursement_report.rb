4fac9608607a8050146d2209f314f140c2fd0719
jay3126 <janmejay.rai@intellecap.com>
2012-10-13 14:46:21 +0530

    Outreach Loan Disbursement report completed. This report has 7 sections :-     1. Loan Disbursement by Caste     2. Loan Disbursement By Religion     3. Loan Disbursement by Loan Cycle     4. Loan Disbursement by Loan Product     5. Loan Disbursement by Branch     6. Loan Disbursement by Town Classification     7. Loan Disbursement by PSL

--- app/models/reports/outreach_loan_disbursement_report.rb
+++ app/models/reports/outreach_loan_disbursement_report.rb
@@ -31,7 +31,8 @@ class OutreachLoanDisbursementReport < Report
   end
 
   def generate
-    data = {:loan_disbursement_by_caste => {}, :loan_disbursement_by_religion => {}}
+    location_facade  = get_location_facade(@user)
+    data = {:loan_disbursement_by_caste => {}, :loan_disbursement_by_religion => {}, :loan_disbursement_by_loan_cycle => {}, :loan_disbursement_by_caste => {}, :loan_disbursement_by_loan_product => {}, :loan_disbursement_by_branch => {}, :loan_disbursement_by_classification => {}, :loan_disbursement_by_psl => {}}
 
     #loan_disbursement_by_caste
     caste_master_list = Constants::Masters::CASTE_CHOICE
@@ -43,7 +44,7 @@ class OutreachLoanDisbursementReport < Report
       loans_disbursed_during_period_per_caste.each do |loan|
         total_pos_per_caste += loan.actual_principal_outstanding(@to_date)
       end
-      data[:loan_disbursement_by_caste][caste] = {:caste_name => caste_name, :total_pos => total_pos_per_caste, :disbursed_loan_count => disbursed_loan_count}
+      data[:loan_disbursement_by_caste][caste] = {:caste_name => caste_name, :total_pos_per_caste => total_pos_per_caste, :disbursed_loan_count => disbursed_loan_count}
     end
 
     #loan_disbursement_by_religion
@@ -56,9 +57,70 @@ class OutreachLoanDisbursementReport < Report
       loans_disbursed_during_period_per_religion.each do |loan|
         total_pos_per_religion += loan.actual_principal_outstanding(@to_date)
       end
-      data[:loan_disbursement_by_religion][religion] = {:religion_name => religion_name, :total_pos => total_pos_per_religion, :disbursed_loan_count => disbursed_loan_count}
+      data[:loan_disbursement_by_religion][religion] = {:religion_name => religion_name, :total_pos_per_religion => total_pos_per_religion, :disbursed_loan_count => disbursed_loan_count}
     end
 
+    #loan disbursement by loan cycle.
+    cycle_number_master = Lending.all.aggregate(:cycle_number)
+    cycle_number_master.each do |loan_cycle_number|
+      loans_disbursed_during_period_per_loan_cycle = Lending.all(:disbursal_date.gte => @from_date, :disbursal_date.lte => @to_date, :cycle_number => loan_cycle_number)
+      disbursed_loan_count = loans_disbursed_during_period_per_loan_cycle.count
+      total_amount_disbursed = MoneyManager.get_money_instance(Money.new(loans_disbursed_during_period_per_loan_cycle.aggregate(:disbursed_amount.sum).to_i, :INR).to_s)
+
+      data[:loan_disbursement_by_loan_cycle][loan_cycle_number] = {:cycle_number => loan_cycle_number, :disbursed_loan_count => disbursed_loan_count, :total_amount_disbursed => total_amount_disbursed}
+    end
+
+    #loan_disbursement_by_loan_product
+    loan_product_master_list = LendingProduct.all
+    loan_product_master_list.each do |loan_product|
+      loans_disbursed_during_period_per_loan_product = Lending.all(:disbursal_date.gte => @from_date, :disbursal_date.lte => @to_date, :lending_product_id => loan_product.id)
+      total_pos_per_loan_product = MoneyManager.default_zero_money
+      disbursed_loan_count = loans_disbursed_during_period_per_loan_product.count
+      loans_disbursed_during_period_per_loan_product.each do |loan|
+        total_pos_per_loan_product += loan.actual_principal_outstanding(@to_date)
+      end
+      data[:loan_disbursement_by_loan_product][loan_product] = {:loan_product_name => loan_product.name, :total_pos_per_loan_product => total_pos_per_loan_product, :disbursed_loan_count => disbursed_loan_count}
+    end
+
+    #loan_disbursement_by_branch
+    branch_master_list = location_facade.all_nominal_branches
+    branch_master_list.each do |branch|
+      loans_disbursed_during_period_per_branch = Lending.all(:disbursal_date.gte => @from_date, :disbursal_date.lte => @to_date, :accounted_at_origin => branch.id)
+      total_pos_per_branch = MoneyManager.default_zero_money
+      disbursed_loan_count = loans_disbursed_during_period_per_branch.count
+      loans_disbursed_during_period_per_branch.each do |loan|
+        total_pos_per_branch += loan.actual_principal_outstanding(@to_date)
+      end
+      data[:loan_disbursement_by_branch][branch] = {:branch_name => branch.name, :total_pos_per_branch => total_pos_per_branch, :disbursed_loan_count => disbursed_loan_count}
+    end
+
+    #loan_disbursement_by_classification
+    town_classification_master_list = Constants::Masters::TOWN_CLASSIFICATION
+    town_classification_master_list.each do |classification|
+      classification_name = classification.to_s.humanize
+      loans_disbursed_during_period_per_classification = Lending.all(:disbursal_date.gte => @from_date, :disbursal_date.lte => @to_date).select{|ld| ld.borrower_town_classification == classification}
+      total_pos_per_classification = MoneyManager.default_zero_money
+      disbursed_loan_count = loans_disbursed_during_period_per_classification.count
+      loans_disbursed_during_period_per_classification.each do |loan|
+        total_pos_per_classification += loan.actual_principal_outstanding(@to_date)
+      end
+      data[:loan_disbursement_by_classification][classification] = {:classification_name => classification_name, :total_pos_per_classification => total_pos_per_classification, :disbursed_loan_count => disbursed_loan_count}
+    end
+
+    #loan_disbursement_by_psl
+    psl_master = PrioritySectorList.all.map{|psl| psl.id}
+    psl_master_list = [nil] + psl_master
+    psl_master_list.each do |psl|
+      psl_name = (psl != nil) ? PrioritySectorList.get(psl).name : "Not Specified"
+      loans_disbursed_during_period_per_psl = Lending.all(:disbursal_date.gte => @from_date, :disbursal_date.lte => @to_date).select{|ld| ld.borrower_psl == psl}
+      total_pos_per_psl = MoneyManager.default_zero_money
+      disbursed_loan_count = loans_disbursed_during_period_per_psl.count
+      loans_disbursed_during_period_per_psl.each do |loan|
+        total_pos_per_psl += loan.actual_principal_outstanding(@to_date)
+      end
+      data[:loan_disbursement_by_psl][psl] = {:psl_name => psl_name, :total_pos_per_psl => total_pos_per_psl, :disbursed_loan_count => disbursed_loan_count}
+    end
+    
     return data
   end
 end
