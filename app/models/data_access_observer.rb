898c9af0a8ee6d24629258cf048f230114345d83
jay3126 <janmejay.rai@intellecap.com>
2012-10-15 16:09:52 +0530

    Audit Trail crash on new database fixed

--- app/models/data_access_observer.rb
+++ app/models/data_access_observer.rb
@@ -1,6 +1,6 @@
 class DataAccessObserver
   include DataMapper::Observer
-  observe *(DataMapper::Model.descendants.to_a - [AuditTrail, LoanDueStatus, ClientAdministration, LoanAuthorization, ClientVerification, LoanFile, LoanApplication, FundingLineAddition, Securitization, Encumberance, BaseScheduleLineItem, LoanBorrower, LoanStatusChange, LoanBaseSchedule, LoanAdministration, LoginInstance, Voucher, Ledger, CostCenter, AccountingRule, OverlapReportResponse, Upload] + [BizLocation, ClientGroup, Client, Lending, PaymentTransaction, StaffMember, SimpleFeeProduct, SimpleInsurancePolicy, CenterCycle, Mfi, LocationLevel, Funder, FundingLine, Tranch, ThirdParty, LoanPurpose, Occupation, ClientGroup, PrioritySectorList, PslSubCategory, DocumentType, StockRegister, AssetCategory, AssetSubCategory, AssetType, Reason, AssetRegister, LocationHoliday, ChequeBook]).uniq # strange bug where observer drops some of the descnedants.
+  observe *(DataMapper::Model.descendants.to_a - [AuditTrail, LoanDueStatus, ClientAdministration, LoanAuthorization, ClientVerification, LoanFile, LoanApplication, FundingLineAddition, Securitization, Encumberance, BaseScheduleLineItem, LoanBorrower, LoanStatusChange, LoanBaseSchedule, LoanAdministration, LoginInstance, Voucher, Ledger, CostCenter, AccountingRule, OverlapReportResponse, Upload, JournalType] + [BizLocation, ClientGroup, Client, Lending, PaymentTransaction, StaffMember, SimpleFeeProduct, SimpleInsurancePolicy, CenterCycle, Mfi, LocationLevel, Funder, FundingLine, Tranch, ThirdParty, LoanPurpose, Occupation, ClientGroup, PrioritySectorList, PslSubCategory, DocumentType, StockRegister, AssetCategory, AssetSubCategory, AssetType, Reason, AssetRegister, LocationHoliday, ChequeBook]).uniq # strange bug where observer drops some of the descnedants.
   
   def self.insert_session(id)
     @_session = ObjectSpace._id2ref(id)
@@ -34,10 +34,12 @@ class DataAccessObserver
         end
         return if diff.length==0
         model = (/Lending$/.match(obj.class.to_s) ? "Lending" : obj.class.to_s)
-        user_role = @_user.staff_member.designation.role_class
-        log = AuditTrail.new(:auditable_id => obj.id, :action => @action, :changes => diff.to_yaml, :type => :log, :user_role => user_role,
-                             :auditable_type => model, :user => @_user, :created_at => DateTime.now)
-        log.save
+        unless @_user.nil?
+          user_role = @_user.staff_member.designation.role_class
+          log = AuditTrail.new(:auditable_id => obj.id, :action => @action, :changes => diff.to_yaml, :type => :log, :user_role => user_role,
+                               :auditable_type => model, :user => @_user, :created_at => DateTime.now)
+          log.save
+        end
       end
     rescue Exception => e
       p diff if diff
