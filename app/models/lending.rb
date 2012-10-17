a680630e234ef4aaea6f101fe22d6223cbf8aafa
Kirti parihar <pariharkirti24@gmail.com>
2012-10-17 17:38:33 +0530

    Securitizations: in loan aligibility- if loan is add. enc then can sec or enc both applied this validation

--- app/models/lending.rb
+++ app/models/lending.rb
@@ -888,11 +888,14 @@ class Lending
 
   def self.is_loan_eligible_for_loan_assignments?(loan_id)
     loan_assignment_facade = FacadeFactory.instance.get_instance(FacadeFactory::LOAN_ASSIGNMENT_FACADE, User.first)
+    loan_assignment = loan_assignment_facade.get_loan_assigned_to(loan_id, Date.today)
+    eligible_to_assign = true
     loan = Lending.get loan_id
     client = Client.get(loan.loan_borrower.counterparty_id)
-    is_inactive = !Client.is_claim_processing_or_inactive?(client)
+    is_active = !Client.is_claim_processing_or_inactive?(client)
     has_3_minimum_repayments = loan.loan_receipts.size >= 3 ? true : false
-    return loan_assignment_facade.get_loan_assigned_to(loan_id, Date.today).nil? && loan.is_outstanding? && is_inactive && has_3_minimum_repayments ? true : false
+    eligible_to_assign = loan_assignment.is_additional_encumbered ? true : false unless loan_assignment.blank?
+    return eligible_to_assign && loan.is_outstanding? && is_active && has_3_minimum_repayments ? true : false
   end
 
   def update_loan_shechdule_according_calendar_holiday(on_date, move_date, after_date = Date.today)
