5fbd64ae2b4fe9f94602d17253010280b39adb0b
Kirti parihar <pariharkirti24@gmail.com>
2012-10-22 11:30:59 +0530

    undefined method `loan_borrower' for nil:NilClass.RESOLVED

--- app/controllers/securitizations.rb
+++ app/controllers/securitizations.rb
@@ -164,7 +164,7 @@ class Securitizations < Application
               assignment_type    = data[:assignment_type]
               assignment_type_id = data[:assignment_type_id]
               # VALIDATIONS
-              is_loan_eligible, reason = loan_facade.is_loan_eligible_for_loan_assignments?(id)
+              is_loan_eligible, reason = (id == 0) ? [false, "Loan ID must not be blank"] : loan_facade.is_loan_eligible_for_loan_assignments?(id)
               if reason.blank?
                 msg << "Effective on date must not be blank" if effective_on_date.blank?
 
@@ -210,7 +210,7 @@ class Securitizations < Application
                   end
                 end
               else
-                msg << "Loan is not eligible for assignment( #{reason} )"
+                msg << "Loan is not eligible for assignment (#{reason})"
               end
             
               begin
