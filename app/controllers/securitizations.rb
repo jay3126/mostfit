53e64bdc4b0d13781f49ca344096051077ae3b83
Kirti parihar <pariharkirti24@gmail.com>
2012-10-22 10:26:01 +0530

    Securitizations: Some validations added.

--- app/controllers/securitizations.rb
+++ app/controllers/securitizations.rb
@@ -149,86 +149,89 @@ class Securitizations < Application
         FasterCSV.open(file_to_write, "w") do |fastercsv|
           fastercsv << [ 'Loan ID', 'Effective On', 'Funder ID', 'Funding line ID', 'Tranch ID', 'Assignment type', 'Assignment type ID', 'Status', 'Error' ]
           record_no = 0
-          loans_data.each do |id, data|
-            msg = []
-            record_no += 1
-            loan_assignment = loan_assignment_facade.get_loan_assigned_to(id, Date.today)
-            loan_status, loan_error = "Not known", "Not known"
-            effective_on_date  = data[:effective_on_date]
-            funder_id          = data[:funder_id]
-            funding_line_id    = data[:funding_line_id]
-            tranch_id          = data[:tranch_id]
-            assignment_type    = data[:assignment_type]
-            assignment_type_id = data[:assignment_type_id]
+          if CSV.readlines(fq_file_path).size <= 1
+            raise "File must not be blank"
+          else
+            loans_data.each do |id, data|
+              msg = []
+              record_no += 1
+              loan_assignment = loan_assignment_facade.get_loan_assigned_to(id, Date.today)
+              loan_status, loan_error = "Not known", "Not known"
+              effective_on_date  = data[:effective_on_date]
+              funder_id          = data[:funder_id]
+              funding_line_id    = data[:funding_line_id]
+              tranch_id          = data[:tranch_id]
+              assignment_type    = data[:assignment_type]
+              assignment_type_id = data[:assignment_type_id]
+              # VALIDATIONS
+              is_loan_eligible, reason = loan_facade.is_loan_eligible_for_loan_assignments?(id)
+              if reason.blank?
+                msg << "Effective on date must not be blank" if effective_on_date.blank?
 
-            # VALIDATIONS
-            is_loan_eligible, reason = loan_facade.is_loan_eligible_for_loan_assignments?(id)
-            if reason.blank?
-              msg << "Effective on date must not be blank" if effective_on_date.blank?
+                funder = NewFunder.get funder_id
+                msg << "Funder ID not found" if funder.blank?
 
-              funder = NewFunder.get funder_id
-              msg << "Funder ID not found" if funder.blank?
+                funding_line = NewFundingLine.get funding_line_id
+                msg << "Funding Line ID not found" if funding_line.blank?
 
-              funding_line = NewFundingLine.get funding_line_id
-              msg << "Funding Line ID not found" if funding_line.blank?
+                tranch = NewTranch.get tranch_id
+                msg << "Tranch ID not found" if tranch.blank?
 
-              tranch = NewTranch.get tranch_id
-              msg << "Tranch ID not found" if tranch.blank?
+                msg << "Assignment type must not be blank" if assignment_type.blank?
 
-              msg << "Assignment type must not be blank" if assignment_type.blank?
-
-              msg << "Assignment type id must not be blank" if assignment_type_id.blank?
+                msg << "Assignment type id must not be blank" if assignment_type_id.blank?
               
-              unless funder.blank? || funding_line.blank? || tranch.blank?
-                msg << "No relation with exists between Funder ID - Funding Line ID - Tranch ID" if tranch.new_funding_line.id != funding_line.id || funding_line.new_funder.id != funder.id
-              end
-              unless assignment_type.blank? || assignment_type_id.blank?
-                if (assignment_type != "s" && assignment_type != "e" && assignment_type != "ae")
-                  msg << "Assignment type: #{assignment_type} is not defined.(Use 's' for Securitization and 'e' for Encumbrance and 'ae' for Additional Encumbrance)"
-                else
-                  if (loan_assignment && loan_assignment.is_additional_encumbered)
-                    if assignment_type == "s"
-                      assignment_type_object = Securitization.get assignment_type_id
-                      msg << "No Securitization found with Id #{assignment_type_id}" if assignment_type_object.blank?
-                    else
-                      assignment_type_object = Encumberance.get assignment_type_id
-                      msg << "No Encumbrance found with Id #{assignment_type_id}" if assignment_type_object.blank?
-                    end
+                unless funder.blank? || funding_line.blank? || tranch.blank?
+                  msg << "No relation with exists between Funder ID - Funding Line ID - Tranch ID" if tranch.new_funding_line.id != funding_line.id || funding_line.new_funder.id != funder.id
+                end
+                unless assignment_type.blank? || assignment_type_id.blank?
+                  if (assignment_type != "s" && assignment_type != "e" && assignment_type != "ae")
+                    msg << "Assignment type: #{assignment_type} is not defined.(Use 's' for Securitization and 'e' for Encumbrance and 'ae' for Additional Encumbrance)"
                   else
-                    if (assignment_type == "s" && tranch.assignment_type == "securitization")
-                      assignment_type_object = Securitization.get assignment_type_id
-                      msg << "No Securitization found with Id #{assignment_type_id}" if assignment_type_object.blank?
-                    elsif (assignment_type == "e" && tranch.assignment_type == "encumbrance") || (assignment_type == "ae")
-                      assignment_type_object = Encumberance.get assignment_type_id
-                      msg << "No Encumbrance found with Id #{assignment_type_id}" if assignment_type_object.blank?
+                    if (loan_assignment && loan_assignment.is_additional_encumbered)
+                      if assignment_type == "s"
+                        assignment_type_object = Securitization.get assignment_type_id
+                        msg << "No Securitization found with Id #{assignment_type_id}" if assignment_type_object.blank?
+                      else
+                        assignment_type_object = Encumberance.get assignment_type_id
+                        msg << "No Encumbrance found with Id #{assignment_type_id}" if assignment_type_object.blank?
+                      end
                     else
-                      msg << "Tranch ID: #{tranch.id} can only be used for #{tranch.assignment_type}"
+                      if (assignment_type == "s" && tranch.assignment_type == "securitization")
+                        assignment_type_object = Securitization.get assignment_type_id
+                        msg << "No Securitization found with Id #{assignment_type_id}" if assignment_type_object.blank?
+                      elsif (assignment_type == "e" && tranch.assignment_type == "encumbrance") || (assignment_type == "ae")
+                        assignment_type_object = Encumberance.get assignment_type_id
+                        msg << "No Encumbrance found with Id #{assignment_type_id}" if assignment_type_object.blank?
+                      else
+                        msg << "Tranch ID: #{tranch.id} can only be used for #{tranch.assignment_type}"
+                      end
                     end
                   end
                 end
+              else
+                msg << "Loan is not eligible for assignment( #{reason} )"
               end
-            else
-              msg << "Loan is not eligible for assignment( #{reason} )"
-            end
             
-            begin
-              if msg.blank?
-                additional_encumbered = assignment_type == "ae" ? true : false
-                loan_assignment_facade.assign_on_date(id, assignment_type_object, effective_on_date, funder_id, funding_line_id, tranch_id, additional_encumbered)
-                loan_facade.assign_tranch_to_loan(id, funding_line_id, tranch_id, applied_by_staff, effective_on_date, recorded_by_user)
-                @sucessfully_uploaded_records += 1
-                loan_status, loan_error = "Success", ''
-              end
-              unless msg.blank?
+              begin
+                if msg.blank?
+                  additional_encumbered = assignment_type == "ae" ? true : false
+                  loan_assignment_facade.assign_on_date(id, assignment_type_object, effective_on_date, funder_id, funding_line_id, tranch_id, additional_encumbered)
+                  loan_facade.assign_tranch_to_loan(id, funding_line_id, tranch_id, applied_by_staff, effective_on_date, recorded_by_user)
+                  @sucessfully_uploaded_records += 1
+                  loan_status, loan_error = "Success", ''
+                end
+                unless msg.blank?
+                  @failed_records += 1
+                  @errors << "Loan ID #{id}: #{msg.flatten.join(', ')}"
+                end
+              rescue => ex
                 @failed_records += 1
-                @errors << "Loan ID #{id}: #{msg.flatten.join(', ')}"
+                @errors << "Loan ID #{id}: #{ex.message}, #{msg.flatten.join(', ')}"
+                loan_status, loan_error = 'Failure', "#{ex.message}, #{msg.flatten.join(', ')}"
               end
-            rescue => ex
-              @failed_records += 1
-              @errors << "Loan ID #{id}: #{ex.message}, #{msg.flatten.join(', ')}"
-              loan_status, loan_error = 'Failure', "#{ex.message}, #{msg.flatten.join(', ')}"
+              fastercsv << [id, effective_on_date, funder_id, funding_line_id, tranch_id, assignment_type, assignment_type_id, loan_status, loan_error]
             end
-            fastercsv << [id, effective_on_date, funder_id, funding_line_id, tranch_id, assignment_type, assignment_type_id, loan_status, loan_error]
           end
         end
       rescue => ex
