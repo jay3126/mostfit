ba2af6609d7aa66707a361c3a340463b2cf1ba69
Kirti parihar <pariharkirti24@gmail.com>
2012-12-18 11:32:50 +0530

    LoanStatusReport: modified and applied pagination.

--- app/models/reports/loan_status_report.rb
+++ app/models/reports/loan_status_report.rb
@@ -1,6 +1,6 @@
 class LoanStatusReport < Report
 
-  attr_accessor :from_date, :to_date, :funding_line_id
+  attr_accessor :from_date, :to_date, :funding_line_id, :page
 
   validates_with_method :funding_line_id, :funding_line_not_selected
 
@@ -9,6 +9,8 @@ class LoanStatusReport < Report
     @to_date   = (dates and dates[:to_date]) ? dates[:to_date] : Date.today
     @name = "Loan Status Report from #{@from_date} to #{@to_date}"
     @user = user
+    @page = params.blank? || params[:page].blank? ? 1 : params[:page]
+    @limit = 10
     get_parameters(params, user)
   end
 
@@ -18,10 +20,6 @@ class LoanStatusReport < Report
 
   def self.name
     "Loan Status Report"
-  end
-
-  def get_reporting_facade(user)
-    @reporting_facade ||= FacadeFactory.instance.get_instance(FacadeFactory::REPORTING_FACADE, user)
   end
 
   def get_location_facade(user)
@@ -34,15 +32,15 @@ class LoanStatusReport < Report
 
   def generate
 
-    reporting_facade = get_reporting_facade(@user)
-    location_facade  = get_location_facade(@user)
+    reporting_facade ||= FacadeFactory.instance.get_instance(FacadeFactory::REPORTING_FACADE, @user)
     data = {}
 
     loan_ids = FundingLineAddition.all(:funding_line_id => @funding_line_id).aggregate(:lending_id)
-    loan_ids_with_disbursal_dates = Lending.all(:id => loan_ids, :disbursal_date.gte => @from_date, :disbursal_date.lte => @to_date).aggregate(:id)
+    loan_ids_with_disbursal_dates = Lending.all(:id => loan_ids, :disbursal_date.gte => @from_date, :disbursal_date.lte => @to_date).to_a.paginate(:page => @page, :per_page => @limit)
+    data[:loan_ids] = loan_ids_with_disbursal_dates
+    data[:loans] = {}
 
-    loan_ids_with_disbursal_dates.each do |l|
-      loan = Lending.get(l)
+    loan_ids_with_disbursal_dates.each do |loan|
       client = loan.loan_borrower.counterparty
       client_id = client ? client.id : "Not Specified"
       client_name = client ? client.name : "Not Specified"
@@ -101,7 +99,7 @@ class LoanStatusReport < Report
       number_of_days_interest_overdue = loan.days_past_dues_on_date(@from_date)
       current_interest_outstanding = loan.actual_interest_outstanding
 
-      data[loan] = {:client_id => client_id, :client_name => client_name, :loan_id => loan_id, :district_id => district_id, :district_name => district_name, :branch_id => branch_id, :branch_name => branch_name, :no_of_installments_remaining => no_of_installments_remaining, :principal_outstanding_beginning_of_week => principal_outstanding_beginning_of_week, :principal_due_during_week => principal_due_during_week, :principal_paid_during_week => principal_paid_during_week, :principal_overdue => principal_overdue, :number_of_days_principal_overdue => number_of_days_principal_overdue, :current_principal_outstanding => current_principal_outstanding, :interest_outstanding_beginning_of_week => interest_outstanding_beginning_of_week, :interest_due_during_week => interest_due_during_week, :interest_paid_during_week => interest_paid_during_week, :interest_overdue => interest_overdue, :number_of_days_interest_overdue => number_of_days_interest_overdue, :current_interest_outstanding => current_interest_outstanding, :loan_lan => loan_lan}
+      data[:loans][loan] = {:client_id => client_id, :client_name => client_name, :loan_id => loan_id, :district_id => district_id, :district_name => district_name, :branch_id => branch_id, :branch_name => branch_name, :no_of_installments_remaining => no_of_installments_remaining, :principal_outstanding_beginning_of_week => principal_outstanding_beginning_of_week, :principal_due_during_week => principal_due_during_week, :principal_paid_during_week => principal_paid_during_week, :principal_overdue => principal_overdue, :number_of_days_principal_overdue => number_of_days_principal_overdue, :current_principal_outstanding => current_principal_outstanding, :interest_outstanding_beginning_of_week => interest_outstanding_beginning_of_week, :interest_due_during_week => interest_due_during_week, :interest_paid_during_week => interest_paid_during_week, :interest_overdue => interest_overdue, :number_of_days_interest_overdue => number_of_days_interest_overdue, :current_interest_outstanding => current_interest_outstanding, :loan_lan => loan_lan}
     end
     data
   end
