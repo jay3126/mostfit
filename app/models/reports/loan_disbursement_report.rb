624026e352010234b09069eac2e0f88af7f98abc
Janmejay Rai <janmejay.rai@intellecap.net>
2013-01-07 15:46:39 +0530

    added interest rate column in Loan Disbursement Report

--- app/models/reports/loan_disbursement_report.rb
+++ app/models/reports/loan_disbursement_report.rb
@@ -31,6 +31,7 @@ class LoanDisbursementReport < Report
       loan_amount = (loan and loan.amount) ? loan.amount : "Not Specified"
       loan_disbursal_date = (loan and loan.disbursal_date) ? loan.disbursal_date : "Not Disbursed Yet"
       loan_purpose = (loan and loan.occupation and (not loan.occupation.nil?)) ? loan.occupation.name : "Loan Purpose not specified"
+      loan_interest_rate = (loan and loan.interest_rate) ? loan.interest_rate : "Interest Rate not specified"
 
       client = Client.get(loan.client_id)
       client_id = client.id
@@ -46,7 +47,7 @@ class LoanDisbursementReport < Report
       branch_name = branch.name
 
       data[loan] = {:branch_id => branch_id, :branch_name => branch_name, :center_id => center_id, :center_name => center_name, :client_id => client_id, :client_name => client_name,
-        :client_group => client_group, :loan_id => loan_id, :loan_amount => loan_amount, :loan_disbursal_date => loan_disbursal_date, :loan_purpose => loan_purpose
+        :client_group => client_group, :loan_id => loan_id, :loan_amount => loan_amount, :loan_disbursal_date => loan_disbursal_date, :loan_purpose => loan_purpose, :loan_interest_rate => loan_interest_rate
       }
     end
     data
