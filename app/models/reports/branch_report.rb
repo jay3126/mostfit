2d81e99b3332370e905348035b2fe64b5c0d2891
Janmejay Rai <janmejay.rai@intellecap.net>
2013-01-18 14:32:58 +0530

    Branch report - small fix

--- app/models/reports/branch_report.rb
+++ app/models/reports/branch_report.rb
@@ -56,7 +56,7 @@ class BranchReport < Report
           staff_name = staff.name
           centers_members_total = reporting_facade.locations_managed_by_staffs_on_date(staff.id, @date)
           outstanding_and_overdue_amounts = reporting_facade.sum_all_outstanding_and_overdues_loans_location_centers_on_date(@date, centers_members_total[:new_location_ids].flatten)
-          arrears = outstanding_and_overdue_amounts[:total_overdue]
+          arrears = outstanding_and_overdue_amounts[:overdue_amounts]
           data[:branches][staff] = {:branch_id => branch_id, :branch_name => branch_name, :staff_name => staff_name, :staff_id => staff_id, :centers_members_total => centers_members_total, :outstanding_and_overdue_amounts => outstanding_and_overdue_amounts, :arrears => arrears, :number_of_loan_accounts_in_arrear => number_of_loan_accounts_in_arrear}
         end
       end
