5d56545f285df1940afc07cde42b5773d2ed9f45
Kaushal Kishor Sharma <kaushal_kishor16@yahoo.com>
2013-01-22 03:15:35 +0530

    reduce loading timing of "Branch Date Wise DCA Report"

--- app/models/reports/branch_date_wise_dca_report.rb
+++ app/models/reports/branch_date_wise_dca_report.rb
@@ -38,49 +38,59 @@ class BranchDateWiseDcaReport < Report
     branches         = @biz_location_branch.class == Array ? @biz_location_branch : [@biz_location_branch]
     preclosure_loans = LoanStatusChange.status_between_dates(LoanLifeCycle::REPAID_LOAN_STATUS, @from_date, @to_date).lending
     branches.each do |branch_id|
-      location                = BizLocation.get branch_id
-      branch_name             = location.name
-      branch_preclosure_loans = preclosure_loans.select{|lending| lending.accounted_at_origin == branch_id}
+      branch = BizLocation.get branch_id
+      branch_name = branch.name
+      branch_loans = LoanAdministration.get_loan_ids_group_vise_accounted_for_date_range_by_sql(branch_id, @from_date, @to_date)
+      disbursed_loan_ids = branch_loans[:disbursed_loan_status].blank? ? [] : branch_loans[:disbursed_loan_status]
+      preclosure_loan_ids = branch_loans[:preclosed_loan_status].blank? ? [] : branch_loans[:preclosed_loan_status]
+      total_loans = disbursed_loan_ids + preclosure_loan_ids
+      loan_receipts = LoanReceipt.all(:lending_id => disbursed_loan_ids)
+      preclose_loan_receipts = LoanReceipt.all(:lending_id => preclosure_loans)
+      total_disbursed = Lending.all(:id => disbursed_loan_ids).aggregate(:disbursed_amount.sum) rescue []
+      total_disbured_amt = total_disbursed.blank? ? MoneyManager.default_zero_money : MoneyManager.get_money_instance_least_terms(total_disbursed.to_i)
+      loan_schedules = BaseScheduleLineItem.all( 'loan_base_schedule.lending_id' => total_loans, :on_date.gte => @from_date, :on_date.lte => @to_date)
+
+      disbursed_loans =  Lending.all(:fields => [:id, :disbursed_amount,:disbursal_date], :disbursal_date.gte => @from_date, :disbursal_date.lte => @to_date, :id => disbursed_loan_ids)
       data[branch_id] = {}
       (@from_date..@to_date).each do |on_date|
+        loan_schedules_on_date = loan_schedules.select{|s| s.on_date == on_date}
 
-        preclosure_collect = interest_accured     = disbursed_amount       = outstanding_principal = emi_collect_interest = MoneyManager.default_zero_money
-        dues_emi_interest  = dues_emi_total       = dues_emi_principal     = emi_collect_principal = emi_collect_total = MoneyManager.default_zero_money
-        fee_collect        = total_fee_collection = preclosure_fee_collect = MoneyManager.default_zero_money
-        unless check_holiday_on_date(on_date)
+        scheduled_principal = loan_schedules_on_date.blank? ? MoneyManager.default_zero_money : MoneyManager.get_money_instance_least_terms(loan_schedules_on_date.map(&:scheduled_principal_due).sum.to_i)
+        scheduled_interest = loan_schedules_on_date.blank? ? MoneyManager.default_zero_money : MoneyManager.get_money_instance_least_terms(loan_schedules_on_date.map(&:scheduled_interest_due).sum.to_i)
+        scheduled_total = scheduled_principal + scheduled_interest
 
-          branch_loans = LoanAdministration.get_loans_accounted(branch_id, on_date).compact
-          branch_loans.each do |lending|
-            if lending.is_outstanding_on_date?(on_date)
-              dues_emi_principal    += lending.actual_principal_outstanding(on_date) || MoneyManager.default_zero_money
-              dues_emi_interest     += lending.actual_interest_outstanding(on_date)  || MoneyManager.default_zero_money
-              dues_emi_total        = dues_emi_principal + dues_emi_interest
-              emi_collect_principal += lending.principal_received_on_date(on_date)
-              emi_collect_interest  += lending.interest_received_on_date(on_date)
-              emi_collect_total     = emi_collect_principal + emi_collect_interest
-              interest_accured      = interest_accured      + lending.accrued_interim_interest(@from_date, @to_date)
-              disbursed_amount      = disbursed_amount      + lending.to_money[:disbursed_amount] if lending.disbursal_date == on_date
-              outstanding_principal = outstanding_principal + lending.actual_principal_outstanding(on_date)
-            end
-          end
-        
-          fee_collection         = FeeReceipt.all_paid_loan_fee_receipts_on_accounted_at(branch_id, on_date)
-          fee_collect            = fee_collection[:loan_fee_receipts].blank? ? MoneyManager.default_zero_money  : Money.new(fee_collection[:loan_fee_receipts].map(&:fee_amount).sum.to_i, default_currency)
-          preclosure_fee_collect = fee_collection[:loan_preclousure_fee_receipts].blank? ? MoneyManager.default_zero_money  : Money.new(fee_collection[:loan_preclousure_fee_receipts].map(&:fee_amount).sum.to_i, default_currency)
+        loan_receipt_on_date = loan_receipts.select{|s| s.effective_on == on_date}
+        preclose_loan_receipt_on_date = preclose_loan_receipts.select{|s| s.effective_on == on_date}
 
-          branch_preclosure_loans.each do |lending|
-            status_change      = lending.loan_status_changes(:to_status => LoanLifeCycle::REPAID_LOAN_STATUS, :effective_on => on_date)
-            preclosure_collect += lending.loan_receipts.last.to_money[:principal_received] unless status_change.blank?
-          end
+        loan_receipt_amt_on_date = LoanReceipt.add_up(loan_receipt_on_date)
+        preclose_loan_receipt_amt_on_date = LoanReceipt.add_up(preclose_loan_receipt_on_date)
 
-          total_fee_collection = fee_collect + preclosure_fee_collect + preclosure_collect
-        end
+        received_principal = loan_receipt_amt_on_date[:principal_received]
+        received_interest = loan_receipt_amt_on_date[:interest_received]
+        total_received = received_principal + received_interest
+
+        preclose_principal = preclose_loan_receipt_amt_on_date[:principal_received]
+
+        loan_fee = FeeReceipt.all_paid_loan_fee_receipts_on_accounted_at(branch_id, on_date)
+        processing_fee = loan_fee[:loan_fee_receipts].blank? ? MoneyManager.default_zero_money : loan_fee[:loan_fee_receipts].map(&:fee_money_amount).sum
+        preclose_fee = loan_fee[:loan_preclousure_fee_receipts].blank? ? MoneyManager.default_zero_money : loan_fee[:loan_preclousure_fee_receipts].map(&:fee_money_amount).sum
+
+        total_collection = total_received + preclose_principal + processing_fee + preclose_fee
+        d_loans = disbursed_loans.select{|l| l.disbursal_date == on_date}
+        disbursed_money_amt = d_loans.blank? ? MoneyManager.default_zero_money : MoneyManager.get_money_instance_least_terms(d_loans.map(&:disbursed_amount).sum.to_i)
+
+        loan_receipts_till_date = loan_receipts.select{|r| r.effective_on <= on_date}
+        loan_receipts_amt_till_date = LoanReceipt.add_up(loan_receipts_till_date)
+        principal_amt_till_date = loan_receipts_amt_till_date[:principal_received]
+
+        pos_on_date = total_disbured_amt > principal_amt_till_date ? total_disbured_amt - principal_amt_till_date : MoneyManager.default_zero_money
+
         data[branch_id][on_date] = {
           :branch_name => branch_name, :branch_id => branch_id, :on_date => on_date,
-          :dues_emi_principal => dues_emi_principal , :dues_emi_interest => dues_emi_interest, :dues_emi_total => dues_emi_total,
-          :emi_collect_principal => emi_collect_principal, :emi_collect_interest => emi_collect_interest, :emi_collect_total => emi_collect_total,
-          :loan_fee_collect => fee_collect, :preclosure_collect_fee => preclosure_fee_collect, :preclosure_collect => preclosure_collect, :total_fee_collect => total_fee_collection,
-          :interest_accrued => interest_accured, :disbursed_amount => disbursed_amount, :outstanding_principal => outstanding_principal
+          :dues_emi_principal => scheduled_principal , :dues_emi_interest => scheduled_interest, :dues_emi_total => scheduled_total,
+          :emi_collect_principal => received_principal, :emi_collect_interest => received_interest, :emi_collect_total => total_received,
+          :loan_fee_collect => processing_fee, :preclosure_collect_fee => preclose_fee, :preclosure_collect => preclose_principal, :total_fee_collect => total_collection,
+          :interest_accrued => scheduled_interest, :disbursed_amount => disbursed_money_amt, :outstanding_principal => pos_on_date
         }
       end
     end
