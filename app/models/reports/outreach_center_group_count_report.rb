43f0f5d948c506a50cfc8cb597c5d0129c2a387f
Kaushal Kishor Sharma <kaushal_kishor16@yahoo.com>
2013-01-21 18:36:55 +0530

    Update "Outreach Center Group Count Report" format and code

--- app/models/reports/outreach_center_group_count_report.rb
+++ app/models/reports/outreach_center_group_count_report.rb
@@ -33,17 +33,23 @@ class OutreachCenterGroupCountReport < Report
   def generate
     data = {}
 
-    centers_created_in_date_range = BizLocation.all("location_level.level" => 0, :creation_date.gte => @from_date, :creation_date.lte => @to_date).count
-    centers_count_at_beginning_of_date_range = BizLocation.all("location_level.level" => 0, :creation_date.lt => @from_date).count
+    centers_created_in_date_range = BizLocation.all("location_level.level" => 0, :center_disbursal_date.gte => @from_date, :center_disbursal_date.lte => @to_date).count
+    centers_count_at_beginning_of_date_range = BizLocation.all("location_level.level" => 0, :center_disbursal_date.lt => @from_date).count
     centers_closed_during_date_range = 0
-    centers_count_at_end_of_date_range = BizLocation.all("location_level.level" => 0, :creation_date.lte => @to_date).count
+    centers_count_at_end_of_date_range = BizLocation.all("location_level.level" => 0, :center_disbursal_date.lte => @to_date).count
 
     client_groups_created_in_date_range = ClientGroup.all(:creation_date.gte => @from_date, :creation_date.lte => @to_date).count
     client_groups_count_at_beginning_of_date_range = ClientGroup.all(:creation_date.lt => @from_date).count
     client_groups_closed_during_date_range = 0
     client_groups_count_at_end_of_date_range = ClientGroup.all(:creation_date.lte => @to_date).count
 
-    data = {:centers_created_in_date_range => centers_created_in_date_range, :centers_count_at_beginning_of_date_range => centers_count_at_beginning_of_date_range, :centers_closed_during_date_range => centers_closed_during_date_range, :centers_count_at_end_of_date_range => centers_count_at_end_of_date_range, :client_groups_created_in_date_range => client_groups_created_in_date_range, :client_groups_count_at_beginning_of_date_range => client_groups_count_at_beginning_of_date_range, :client_groups_closed_during_date_range => client_groups_closed_during_date_range, :client_groups_count_at_end_of_date_range => client_groups_count_at_end_of_date_range}
+    client_created_in_date_range = Lending.all(:disbursal_date.gte => @from_date, :disbursal_date.lte => @to_date, :status => LoanLifeCycle::DISBURSED_LOAN_STATUS).count
+    loan_matured_in_date_range = Lending.all(:repaid_on_date.gte => @from_date, :repaid_on_date=> @to_date, :status => LoanLifeCycle::REPAID_LOAN_STATUS).count
+    loan_preclosure_in_date_range = Lending.all(:preclosed_on_date.gte => @from_date, :preclosed_on_date => @to_date, :status => LoanLifeCycle::PRECLOSED_LOAN_STATUS).count
+    all_clients_count = Client.all.count
+    female_clients_count = Client.all(:gender => Constants::Masters::FEMALE_GENDER).count
+
+    data = {:all_clients_count => all_clients_count, :female_clients_count => female_clients_count, :client_created_in_date_range =>  client_created_in_date_range, :loan_matured_in_date_range => loan_matured_in_date_range, :loan_preclosure_in_date_range => loan_preclosure_in_date_range, :centers_created_in_date_range => centers_created_in_date_range, :centers_count_at_beginning_of_date_range => centers_count_at_beginning_of_date_range, :centers_closed_during_date_range => centers_closed_during_date_range, :centers_count_at_end_of_date_range => centers_count_at_end_of_date_range, :client_groups_created_in_date_range => client_groups_created_in_date_range, :client_groups_count_at_beginning_of_date_range => client_groups_count_at_beginning_of_date_range, :client_groups_closed_during_date_range => client_groups_closed_during_date_range, :client_groups_count_at_end_of_date_range => client_groups_count_at_end_of_date_range}
     return data
   end
 end
