5433ed65133aee4f2fabc1c33d21d8b9e849d8ab
Kaushal Kishor Sharma <kaushal_kishor16@yahoo.com>
2013-02-15 13:55:49 +0530

    update daily repayment report add advace and recovery column

--- app/models/reports/daily_repayment_detail_report.rb.rb
+++ app/models/reports/daily_repayment_detail_report.rb.rb
@@ -23,73 +23,16 @@ class DailyRepaymentDetailReport < Report
     data[:repayments] = @paginate.blank? ? LoanReceipt.all(:is_advance_adjusted => false, :effective_on => @date).aggregate(:id).paginate(:page => @page, :per_page => @limit) : LoanReceipt.all(:is_advance_adjusted => false, :effective_on => @date).aggregate(:id)
     data[:loan_payments] = {}
     loan_receipts.group_by{|r| r.accounted_at}.each do |branch_id, receipts|
+      branch_name = BizLocation.get(branch_id).name
       data[:loan_payments][branch_id] = {}
       receipts.group_by{|l| l.lending_id}.each do |lending_id, l_receipts|
         data[:loan_payments][branch_id][lending_id] = {}
         loan = Lending.get(lending_id)
-        l_receipt = LoanReceipt.add_up(l_receipts)
         loan_lan = loan.lan
-        branch_name = BizLocation.get(loan.accounted_at_origin).name
         center_name = BizLocation.get(loan.administered_at_origin).name
-        if l_receipt[:advance_received] > MoneyManager.default_zero_money
-          schedules = BaseScheduleLineItem.all('loan_base_schedule.lending_id' => lending_id, :installment.not => 0, :on_date.gte => @on_date, :order => [:on_date.desc])
-          total_received = l_receipt[:principal_received] + l_receipt[:interest_received] + l_receipt[:advance_received]
-          schedules.each do |schedule|
-            s_principal = MoneyManager.get_money_instance_least_terms(schedule.scheduled_principal_due.to_i)
-            s_interest = MoneyManager.get_money_instance_least_terms(schedule.scheduled_interest_due.to_i)
-
-            i_received = total_received > s_interest ? total_received - s_interest : s_interest - total_received
-            total_received = total_received - i_received
-
-            p_received = total_received > s_principal ? total_received - s_principal : s_principal - total_received
-            total_received = s_principal > p_received ? MoneyManager.default_zero_money : total_received - p_received
-            data[:loan_payments][branch_id][lending_id][schedule.on_date] = {:schedule_date => schedule.on_date, :loan_id => lending_id, :lan_no => loan_lan, :branch_id => branch_id, :branch_name => branch_name, :center_name => center_name, :principal_received => p_received, :interest_received => i_received}
-            break if total_received == MoneyManager.default_zero_money
-          end
-
-        else
-          schedules = BaseScheduleLineItem.all('loan_base_schedule.lending_id' => lending_id, :installment.not => 0, :order => [:on_date])
-          receipts_till_date = loan.loan_receipts(:is_advance_adjusted => false, :effective_on.lt => @date)
-          received_amt_till_date = LoanReceipt.add_up(receipts_till_date)
-          schedules.each do |schedule|
-            s_principal = MoneyManager.get_money_instance_least_terms(schedule.scheduled_principal_due.to_i)
-            s_interest = MoneyManager.get_money_instance_least_terms(schedule.scheduled_interest_due.to_i)
-
-            r_principal = received_amt_till_date[:principal_received]
-            r_interest = received_amt_till_date[:interest_received]
-            received_amt_till_date[:principal_received] = received_amt_till_date[:principal_received] - s_principal if received_amt_till_date[:principal_received] > s_principal
-            received_amt_till_date[:interest_received] =  received_amt_till_date[:interest_received] - s_interest if received_amt_till_date[:interest_received] > s_interest
-
-            if(s_principal > r_principal || s_interest > r_interest)
-              data[:loan_payments][branch_id][lending_id][schedule.on_date] = {}
-
-              aj_principal = received_amt_till_date[:principal_received] != MoneyManager.default_zero_money ? s_principal - received_amt_till_date[:principal_received] : MoneyManager.default_zero_money
-              aj_interest = received_amt_till_date[:interest_received] != MoneyManager.default_zero_money ? s_interest - received_amt_till_date[:interest_received] : MoneyManager.default_zero_money
-              if aj_principal == MoneyManager.default_zero_money
-                p_received = l_receipt[:principal_received] > s_principal ? s_principal : s_principal - l_receipt[:principal_received]
-                l_receipt[:principal_received] = l_receipt[:principal_received] > s_principal ? l_receipt[:principal_received] - s_principal : MoneyManager.default_zero_money
-              else
-                l_receipt[:principal_received] = l_receipt[:principal_received] > aj_principal ? l_receipt[:principal_received] - aj_principal : MoneyManager.default_zero_money
-                p_received = aj_principal
-                received_amt_till_date[:principal_received] = MoneyManager.default_zero_money
-              end
-
-              if aj_interest == MoneyManager.default_zero_money
-                i_received = l_receipt[:interest_received] > s_interest ? s_interest : s_interest - l_receipt[:interest_received]
-                l_receipt[:interest_received] = l_receipt[:interest_received] > s_interest ? l_receipt[:interest_received] - s_interest : MoneyManager.default_zero_money
-              else
-                l_receipt[:interest_received] = l_receipt[:interest_received] > aj_interest ? l_receipt[:interest_received] - aj_interest : MoneyManager.default_zero_money
-                i_received = aj_interest
-                received_amt_till_date[:interest_received] = MoneyManager.default_zero_money
-              end
-
-              data[:loan_payments][branch_id][lending_id][schedule.on_date] = {:schedule_date => schedule.on_date, :loan_id => lending_id, :lan_no => loan_lan, :branch_id => branch_id, :branch_name => branch_name, :center_name => center_name, :principal_received => p_received, :interest_received => i_received}
-
-              break if l_receipt[:principal_received] == MoneyManager.default_zero_money && l_receipt[:interest_received] == MoneyManager.default_zero_money
-            end
-          end
+        l_receipts.each do |l_receipt|
+          data[:loan_payments][branch_id][lending_id][l_receipt.effective_on] = {:schedule_date => l_receipt.effective_on, :loan_id => lending_id, :lan_no => loan_lan, :branch_id => branch_id, :branch_name => branch_name, :center_name => center_name, :principal_received => l_receipt.to_money[:principal_received], :interest_received => l_receipt.to_money[:interest_received], :advance_received => l_receipt.to_money[:advance_received], :recovery_reveived => l_receipt.to_money[:loan_recovery]}
         end
-
       end
     end
     data
@@ -107,7 +50,7 @@ class DailyRepaymentDetailReport < Report
     data[:loan_payments].each do |location_id, b_values|
       b_values.each do |loan_id, l_values|
         l_values.each do |on_date, s_value|
-          value = [s_value[:branch_name], s_value[:center_name], s_value[:lan_no], s_value[:schedule_date], s_value[:principal_received], s_value[:interest_received],s_value[:principal_received]+s_value[:interest_received]]
+          value = [s_value[:branch_name], s_value[:center_name], s_value[:lan_no], s_value[:schedule_date], s_value[:principal_received], s_value[:interest_received],s_value[:advance_received],s_value[:recovery_reveived]]
           append_to_file_as_csv([value], csv_loan_file)
         end
       end
@@ -124,7 +67,7 @@ class DailyRepaymentDetailReport < Report
   end
 
   def headers
-    _headers ||= [["Branch Name", "Center Name", "Loan Account Number", "Schedule Date", "Principal", "Interest", "Amount"]]
+    _headers ||= [["Branch Name", "Center Name", "Loan Account Number", "Schedule Date", "Principal", "Interest", "Advance", "Recovery"]]
   end
 
 end
